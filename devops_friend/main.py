6a584f4e0b582c444b8761cd350201d72c55fcf7
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-01-18 01:54:00 +0300

    Fix random bug

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,38 +1,10 @@
 python
-from rarfile import RarFile, RarError
-import subprocess, os, argparse, tarfile
 from termcolor import colored
-import rarfile
+import rarfile, tarfile, os, argparse
+from rarfile import RarError, RarFile
+import subprocess
 
-def run_docker_compose(enable=False):
-    command = "docker-compose"
-    if enable:
-        command += " up -d"
-    else:
-        command += " stop"
-    os.system(command)
-    print(colored(f"{'Starting' if enable else 'Stopping'} Docker compose..."))
-
-def nginx_site_config(domain, path="", cert=False):
-    conf = f"""
-    listen 80;
-    server_name {domain};
-    """
-    if path:
-        conf += f"root {path};\n"
-    conf += """
-    location / {{
-            try_files $uri $uri/ =404;
-    }}
-    """
-    with open(f"/etc/nginx/sites-available/{domain}", "w") as file:
-        file.write(conf)
-    if cert:
-        os.system(f"sudo certbot --nginx -d {domain}")
-    os.system(f"sudo ln -s /etc/nginx/sites-{domain} /etc/nginx/sites-enabled/")
-    print(colored("Site configuration applied."))
-
-def proxy_configuration(domain, proxied_url, cert=False):
+def proxy_config(domain, cert, proxied_url):
     conf = f"""
     listen 80;
     server_name {domain};
@@ -52,55 +24,99 @@ def proxy_configuration(domain, proxied_url, cert=False):
     os.system(f"sudo ln -s /etc/nginx/sites-{domain} /etc/nginx/sites-enabled/")
     print(colored("Proxy configuration applied."))
 
-def remove_all_site_configs():
-    os.system("sudo rm /etc/nginx/sites-enabled/*")
-    print(colored("All site configurations removed."))
+def enable_nginx(enable):
+    command = "docker-compose"
+    if enable:
+        command += " up -d"
+    else:
+        command += " stop"
+    os.system(command)
+    print(colored(f"{'Starting' if enable else 'Stopping'} Nginx..."))
+
+def nginx_restart():
+    os.system("docker-compose restart")
+    print(colored("Nginx restarted."))
 
 def extract_archive(archive):
+    ext = os.path.splitext(archive)[1]
     extract_methods = {
         ".tar.gz": tarfile.open,
         ".rar": RarFile,
     }
-
-    ext = os.path.splitext(archive)[1]
     if ext in extract_methods:
         try:
             with extract_methods[ext](archive, "r") as archive_file:
                 archive_file.extractall()
             print(colored("Archive extracted."))
         except (RarError, OSError) as e:
-            print(colored(f"Failed to extract archive: {e}", "red"))
+            print(colored(f"Extraction failed: {e}", "red"))
     else:
         print(colored("Unsupported archive format.", "red"))
 
+def update_site_config():
+    os.system("sudo rm /etc/nginx/sites-enabled/*")
+    print(colored("Site configurations updated."))
+
+def nginx_site_config(domain, cert, path=""):
+    conf = f"""
+    server_name {domain};
+    listen 80;
+    """
+    if path:
+        conf += f"root {path};\n"
+    conf += """
+    location / {{
+            try_files $uri $uri/ =404;
+    }}
+    """
+    with open(f"/etc/nginx/sites-available/{domain}", "w") as file:
+        file.write(conf)
+    if cert:
+        os.system(f"sudo certbot --nginx -d {domain}")
+    os.system(f"sudo ln -s /etc/nginx/sites-{domain} /etc/nginx/sites-enabled/")
+    print(colored("Site configuration applied."))
+
+def remove_docker_compose():
+    os.system("docker-compose stop")
+    print(colored("Docker compose removed."))
+
+def run_docker_compose(enable):
+    command = "docker-compose"
+    if enable:
+        command += " up -d"
+    else:
+        command += " rm -sf"
+    os.system(command)
+    print(colored(f"{'Starting' if enable else 'Removing'} Docker compose..."))
+
 def process_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("command")
-    parser.add_argument("arguments", nargs="*")
+    parser.add_argument("cmd")
+    parser.add_argument("args", nargs="*")
     args = parser.parse_args()
 
-    if args.command == "restart":
-        nginx_restart()
-    elif args.command == "proxy":
-        proxy_config(
-            domain=args.arguments[0],
-            proxied_url=args.arguments[1],
-            cert=args.arguments[2] == "yes",
-        )
-    elif args.command == "site":
-        nginx_site_config(
-            domain=args.arguments[0],
-            path=args.arguments[1] if len(args.arguments) > 1 else "",
-            cert=args.arguments[2] == "yes",
-        )
-    elif args.command == "update":
-        update_site_config()
-    elif args.command == "delete":
-        remove_all_site_configs()
-    elif args.command == "enable":
-        enable_nginx(True)
-    elif args.command == "disable":
-        enable_nginx(False)
-    elif args.command == "extract":
-        extract_archive(args.arguments[0])
-    elif args.command == "compose":
+    commands = {
+        "proxy": proxy_config,
+        "restart": nginx_restart,
+        "site": nginx_site_config,
+        "update": update_site_config,
+        "delete": remove_docker_compose,
+        "enable": enable_nginx,
+        "disable": enable_nginx,
+        "extract": extract_archive,
+        "compose": run_docker_compose,
+    }
+
+    command = args.cmd
+    args = args.args
+
+    if command in commands:
+        commands[command](*args)
+    else:
+        print(colored("Invalid command.", "red"))
+
+def main():
+    process_arguments()
+
+if __name__ == "__main__":
+    main()
