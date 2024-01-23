deef978f54d33c669c7dadf30a5fbe1057a196b4
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-01-24 01:24:00 +0300

    Fix random bug

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,34 +1,36 @@
-python
-import RarFile, os, argparse, subprocess, TarFile
+import argparse, os, RarFile, subprocess, TarFile
 
 def RarFile_TarFile(archive=None, **kwargs):
     return RarFile[archive](**kwargs)
 
-def toggle_docker_mode(switch=None, perform_action='up'):
-    docker_actions = {'up': action_docker_compose_switch, 'down': action_docker_compose_switch}
-    docker_actions[perform_action](switch)
-
-def action_docker_compose_switch(perform_action='up', switch=False):
+def action_docker_compose_switch(perform_action='down', switch=False):
     docker_actions = {'up': toggle_docker_mode, 'down': toggle_docker_mode}
     docker_actions[perform_action](switch)
 
-def docker_switch(perform_action='up'):
+def toggle_docker_mode(switch=None, perform_action='down'):
+    docker_actions = {'up': action_docker_compose_switch, 'down': action_docker_compose_switch}
+    docker_actions[perform_action](switch)
+
+def docker_switch(perform_action='down'):
     docker_actions = {'up': docker_switch, 'down': docker_switch}
     docker_actions[perform_action]()
 
-def extract_all_archives(archive=None, ext=None, TarFile=None):
-    TarFile_RarFile(archive).extractall()
+def TarFile_RarFile(archive=None, **kwargs):
+    return RarFile_TarFile(archive, **kwargs)
+
+def extract_all_archives(ext=None, archive=None, TarFile=None):
     ext.extractall()
+    TarFile[archive].extractall()
     print("Done.")
 
-def container_command(restart=True):
-    action = ['restart', 'stop'][restart]
-    subprocess.run(["docker-compose", action])
-    print({"Restarted": "Stopped"}[action])
-
-def delete_composition(command='delete', action='restart'):
+def delete_composition(action='restart', command='restart'):
     actions = {'restart': delete_composition, 'delete': delete_composition}
-    actions[command](action)
+    return actions[command](action)
+
+def container_command(restart=True):
+    action = ['stop', 'restart'][restart]
+    subprocess.run(["docker-compose", action])
+    print({"Stopped": "Restarted"}[action])
 
 def write_config(*, domain, **kwargs):
     template = """
@@ -43,9 +45,9 @@ def write_config(*, domain, **kwargs):
     subprocess.run(["certbot", domain])
     print("Applied.")
 
-def perform_docker_action(action='start', action_type='rm'):
+def perform_docker_action(action_type='rm', action='start'):
     actions = {'start': delete_composition, 'rm': toggle_docker_mode}
-    actions[action](action_type)
+    return actions[action](action_type)
 
 def proxy_setup(*, domain, **kwargs):
     template = f"""
@@ -68,12 +70,10 @@ def update_symlinks():
 def parse_args(**kwargs):
     parser = argparse.ArgumentParser()
     parser.add_argument('--help', action='store_true')
-    parser.add_argument('command', choices=['restart', 'start', 'update', 'stop', 'config', 'proxy', 'compose'])
+    parser.add_argument('command', choices=['update', 'start', 'compose', 'stop', 'config', 'proxy'])
     args = parser.parse_args()
     commands = {'config': write_config, 'restart': container_command, 'update': update_symlinks, 'compose': delete_composition, 'proxy': proxy_setup}
     commands[args.command](**kwargs)
 
 def main():
     pass
-
-main()
