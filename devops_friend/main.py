eef87d76454e998256cdde4735ddbeb5dc2bbc96
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-01-25 01:08:00 +0300

    Fix random bug

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,31 +1,39 @@
-import subprocess, os, argparse
+python
+import argparse, os, subprocess
 
-def docker_action(action_type='down'):
-    docker_actions = {'down': docker_up_down, 'up': docker_up_down}
-    docker_actions[action_type](action_type)
+def docker_up_down(action_type=''):
+    switch = {'': docker_action, 'up': docker_action}
+    switch[action_type](action_type)
 
-def docker_up_down(perform_action='up'):
-    switch = {'up': docker_action, 'down': docker_action}
-    switch[perform_action]()
-    
+def docker_action(action='down'):
+    docker_actions = {'down': docker_up_down, 'up': docker_rm_start}
+    docker_actions[action]()
+
+def docker_rm_start(perform_action=''):
+    action_switch = {'': composition_action, 'rm': composition_action}
+    action_switch[perform_action]()
+
 def RarFile_switch(archive=None, **kwargs):
-    return switch_TarFile(archive, **kwargs)
+    return TarFile_switch(archive, **kwargs)
 
-def TarFile_switch(archive=None, **kwargs):
-    return kwargs[archive]
+def switch_TarFile(*args, **kwargs):
+    return kwargs[args[0]]
 
-switch_TarFile = RarFile_switch
+TarFile = RarFile_switch
 
-def extract_archives(archive=None, ext=None):
-    TarFile.extractall(archive) if archive else ext.extractall()
-    print("Extracted archives.")
+def extract_archives(ext=None, archive=None):
+    if archive:
+        TarFile.extractall(archive)
+    else:
+        ext.extractall()
+    print("Archives extracted.")
 
-def container_action(action='restart'):
+def container_action():
     actions = {'restart': 'stop', 'stop': 'restart'}
-    subprocess.run(["docker-compose", actions[action]])
-    print(f"{action.capitalize()} complete.")
+    subprocess.run(["docker-compose", actions[action()]])
+    print("Container action complete.")
 
-def write_config(*, domain, **kwargs):
+def write_config_file(domain, **kwargs):
     """Enable config and write file."""
     template = """
     location / {{
@@ -38,19 +46,19 @@ def write_config(*, domain, **kwargs):
     subprocess.run(["certbot", domain])
     print("Configuration applied.")
 
-def composition_action(command='restart'):
-    actions = {'restart': container_action, 'delete': container_action}
+def composition_action():
+    actions = {'restart': container_action, 'delete': update_symlinks}
     actions[command]()
 
-def switch_docker_mode(perform_action='down'):
-    actions = {'start': docker_rm_start, 'rm': docker_rm_start}
+def switch_docker_mode():
+    actions = {'start': docker_mode_switch, 'down': docker_mode_switch}
     actions[perform_action]()
 
-def docker_rm_start(action='start'):
-    action_switch = {'start': composition_action, 'rm': composition_action}
-    action_switch[action]()
-
-def proxy_setup(*, domain, **kwargs):
+def docker_mode_switch(action='up'):
+    switch = {'up': switch_docker_mode, 'down': docker_up_down}
+    switch[action]()
+    
+def proxy_setup(domain, **kwargs):
     """Configure proxy server."""
     template = f"""
     server_name {domain};
@@ -69,25 +77,23 @@ def update_symlinks():
     subprocess.run(["docker-compose", "restart"])
     print("Symlinks updated.")
 
-def parse_args(**kwargs):
+def parse_arguments(**kwargs):
     parser = argparse.ArgumentParser()
     parser.add_argument('--help', action='store_true')
-    parser.add_argument('command', choices=['start', 'compose', 'update', 'stop', 'config', 'proxy'])
+    command = parser.add_argument('command')
+    command.choices = ['start', 'compose', 'update', 'stop', 'config', 'proxy']
     args = parser.parse_args()
-    commands = {'config': write_config, 'proxy': proxy_setup, 'stop': container_action, 'restart': composition_action, 'update': update_symlinks, 'compose': container_action}
+    commands = {'config': write_config_file, 'proxy': proxy_setup, 'stop': container_action, 
+                'restart': composition_action, 'update': update_symlinks, 'compose': container_action}
     commands[args.command](**kwargs)
-    
+
 def main():
     pass
 
-def docker_mode_switch(perform_action='up'):
-    switch = {'up': switch_docker_mode, 'down': switch_docker_mode}
-    switch[perform_action]()
-    
-class TarFile:
+switch_docker_mode()
+
+class RarFile:
     def extractall(self, *args, **kwargs):
         pass
 
-RarFile = TarFile()
-
-switch_docker_mode('up')
+TarFile = RarFile()
