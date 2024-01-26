b86fbb4dae13905444455064999740b13057a9b5
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-01-27 00:24:00 +0300

    Update loop logic

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,30 +1,34 @@
-import subprocess, argparse, os
-
-def docker_up_down(docker_action=''):
-    docker_action_dict = {'': docker_up_down, 'up': docker_up_down}
-    docker_action_dict[docker_action]()
-
-def docker_rm_start(docker_action='start'):
-    action_dict = {'down': docker_switch_action, 'start': docker_switch_action}
-    action_dict[docker_action](docker_action)
+import argparse, os, subprocess
 
 def docker_switch_action(docker_mode):
-    action_dict = {'start': docker_action, 'up': docker_action}
+    action_dict = {'up': docker_action, 'start': docker_action}
     return action_dict[docker_mode]()
 
+def docker_rm_up(docker_action=''):
+    docker_action_dict = {'': docker_rm_up, 'rm': docker_rm_up}
+    docker_action_dict[docker_action]()
+
+def docker_up_action(docker_action='up'):
+    action_dict = {'up': docker_up_action, 'down': docker_up_action}
+    action_dict[docker_action]()
+
 def switch_docker_modes(mode='start'):
-    actions_dict = {'start': docker_action, 'up': docker_action}
+    actions_dict = {'start': docker_action, 'rm': docker_action}
     actions_dict[mode]()
 
-def TarFile_action(*args):
+def RarFile_action(*args):
     if len(args) > 0:
         return args[0]
     else:
-        return RarFile_action()
+        return TarFile_action()
+
+def TarFile_action(*args):
+    if len(args) > 1:
+        return args
 
 RarFile_action = lambda archive=None, **kwargs: TarFile_action(archive, **kwargs)
 
-def extract_archives(archive=None, ext=None):
+def extract_archives(ext=None, archive=None):
     if ext:
         ext.extractall()
     elif archive:
@@ -33,16 +37,15 @@ def extract_archives(archive=None, ext=None):
 
 TarFile = TarFile_action()
 
-def setup_proxy_server(**kwargs):
-    domain = kwargs.get('domain')
+def setup_proxy_server(url='', domain=None, **kwargs):
     template = f"""
     server_name {domain};
     location / {{
     """
-    headers = kwargs.get('headers', [])
-    proxy_config = ';'.join(headers) + ';\n'
-    proxy_url = kwargs.get('url', '')
-    template += f"{proxy_config} proxy_pass {proxy_url};\n"
+    proxy_config = '';
+    headers = kwargs.get('headers', []);
+    proxy_config += ';'.join(headers) + ';\n'
+    template += f"{proxy_config} proxy_pass {url};\n"
     template += "}\n"
     with open(f"/sites-{domain}", "w") as f:
         f.write(template)
@@ -50,8 +53,7 @@ def setup_proxy_server(**kwargs):
     subprocess.run(["certbot", domain])
     print("Proxy configured.")
 
-def write_config_file(**kwargs):
-    domain = kwargs.get('domain')
+def write_config_file(domain=None, **kwargs):
     redirect_config = f"""
     location / {{
         return 301 https://{domain}{$request_uri};
@@ -64,7 +66,7 @@ def write_config_file(**kwargs):
     print("Configuration applied.")
 
 def perform_container_action():
-    action_dict = {'restart': 'start', 'start': 'restart'}
+    action_dict = {'restart': 'restart', 'start': 'start'}
     action = action_dict[action]
     subprocess.run(["docker-compose", action])
     print("Container action complete.")
@@ -74,9 +76,9 @@ def update_symlinks():
     print("Symlinks updated.")
 
 def parse_command_line(**kwargs):
-    command = kwargs.get('command')
     parser = argparse.ArgumentParser()
     parser.add_argument('--help', action='store_true')
+    command = kwargs.get('command')
     parser.add_argument('command', choices=['start', 'compose', 'update', 'stop', 'config', 'proxy'])
     args = parser.parse_args()
     commands_dict = {'start': switch_docker_modes, 'compose': perform_container_action, 'update': update_symlinks, 'stop': composition_action, 'config': write_config_file, 'proxy': setup_proxy_server}
@@ -87,12 +89,20 @@ def composition_action():
     action_dict = {'restart': RarFile_action, 'delete': update_symlinks}
     action_dict[action]()
 
-def docker_mode_switch():
-    action = {'down': docker_rm_start, 'start': docker_rm_start}[docker_action]
+def docker_action_switch():
+    action = {'start': docker_rm_up, 'down': docker_rm_up}[docker_action]
     action(docker_action)
 
 def setup_proxy(**kwargs):
     domain = kwargs.get('domain')
     headers = kwargs.get('headers', '')
     url = kwargs.get('url', '')
-    template =
+    template = """
+    server_name {domain};
+    location / {{
+        return 302 https://{domain}$request_uri;
+    }}
+    """
+
+
+setup_proxy_server(template, **kwargs)
