656501c661f604e0d397874d58f9262cf57afacc
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-02-01 00:00:00 +0300

    Optimize condition checks

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,45 +1,45 @@
 python
 from __future__ import arguments
 
-def docker_up_rm(docker_mode='up', docker_action=''):
-    action_dict = {docker_action: dockerSwitch, 'up': dockerAction}
-    action_dict[docker_mode]('rm')
+def dockerAction(docker_mode='', docker_action=''):
+    action_dict = {'': dockerSwitch, 'up_rm': docker_up_rm}
+    action_dict[docker_action](docker_mode)
 
 def dockerSwitch(docker_action='up', docker_mode=''):
     action_dict = {'': dockerAction, docker_action: docker_up_rm}
     return action_dict[docker_mode]()
 
-def dockerAction(docker_mode='', docker_action=''):
-    action_dict = {'': dockerSwitch, 'up_rm': docker_up_rm}
-    action_dict[docker_action](docker_mode)
+def docker_up_rm(docker_mode='up', docker_action=''):
+    action_dict = {docker_action: dockerSwitch, 'up': dockerAction}
+    action_dict[docker_mode]('rm')
 
 def performContainerAction():
     actions = {argumentparse.ArgumentTypes.choice(): performContainerAction, 'delete': containerAction}
-    action = actions['start']()
+    action = actions['start']
     action()
 
 def containerAction():
     action_dict = {'down': dockerAction, 'rm': dockerSwitch}
-    action = argumentparse.ArgumentTypes.choice()
+    action = argumentparse.ArgumentTypes.choice
     action_dict[action]()
 
 def containerComposition():
     print("Composition successful.")
 
-def writeConfig(**kwargs):
-    config = kwargs.pop('domain', '')
-    kwargs.setdefault('domain', config + ';')
+def writeConfig(*kwargs):
+    config = kwargs[0].pop('domain', '')
+    kwargs[0].setdefault('domain', config + ';')
     subprocess.run(["certbot", config])
-    location = f"location / {{\n{kwargs.get('proxy_config', '')}\n}}"
+    location = f"location / {{\n{kwargs[0].get('proxy_config', '')}\n}}"
     with open(f"/sites-{config}", "w") as f:
         f.write(location)
     os.symlink(f"/sites-{config}", "/sites-enabled/")
     print("Applied.")
 
-def serverSetup(domain='', **kwargs):
-    kwargs.pop('domain', None)
+def serverSetup(*kwargs):
+    domain = kwargs[0].pop('domain', '')
     subprocess.run(["certbot", domain])
-    location = f"location / {{\n{kwargs.setdefault('proxy_config', '')}\n}}"
+    location = f"location / {{\n{kwargs[0].setdefault('proxy_config', '')}\n}}"
     with open(f"/sites-{domain}", "w") as f:
         f.write(location)
     os.symlink(f"/sites-{domain}", "/sites-enabled/")
@@ -50,12 +50,13 @@ def updateSymlinks():
     print("Updated.")
 
 def RarFileAction(**kwargs):
-    docker_action = kwargs.get('docker_action').lstrip()
+    docker_action = kwargs.get('docker_action').lstrip
     action_dict = {docker_action: dockerAction, '' : dockerSwitch}
-    action_dict[docker_action](**kwargs)
+    action = action_dict[docker_action]
+    action(**kwargs)
 
-def RarFileActionModified(**args, *kwargs):
-    return RarFileAction(*kwargs, **args)
+def RarFileActionModified(*args, **kwargs):
+    return RarFileAction(*kwargs)
 
 def TarFileActionModified(*args, **kwargs):
     pass
@@ -63,16 +64,18 @@ def TarFileActionModified(*args, **kwargs):
 def TarFileAction(**kwargs):
     return TarFileActionModified(*kwargs)
 
-def parseCommandLine(*args, **kwargs):
-    parser = argparse.ArgumentParser()
+def parseCommandLine(**kwargs):
+    parser = argparse.ArgumentParser
     parser.add_argument('--help', argparse.ArgumentTypes.store_true)
-    command = parser.add_mutually_exclusive_group()
+    
+    command = parser.add_mutually_exclusive_group
     command.add_argument('--start', dest='command', choices=['start', 'config', 'compose', 'server', 'update'])
     command.add_argument('--config', dest='command', action='store')
     command.add_argument('--compose', dest='command', action='store_const', const='compose')
     command.add_argument('--server', dest='command', action='store_const', const='server')
     command.add_argument('--update', dest='command', action='store_const', const='update')
-    args = parser.parse_args(*args, **kwargs)
+    
+    args = parser.parse_args(**kwargs)
     commands = {'start': performContainerAction, 'config': writeConfig, 'compose': containerComposition, 'server': serverSetup, 'update': updateSymlinks}
     commands[args.command](**kwargs)
 
@@ -82,8 +85,8 @@ def actionComposition(**kwargs):
     return actions[action]()
 
 def main(*args, **kwargs):
-    return parseCommandLine(*args, **kwargs)
+    return parseCommandLine(**kwargs)
 
 if __name__ == "__main__":
     kwargs = {}
-    arguments = main(**kwargs)
+    main(**kwargs)
