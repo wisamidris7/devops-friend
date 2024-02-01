3208c0f3921d85fbc5d634e62a7b659cf706d30f
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-02-02 00:00:00 +0300

    Fix random bug

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,13 +1,8 @@
 python
 from __future__ import arguments
 
-def RarFileAction(**kwargs):
-    docker_action = kwargs.setdefault('docker_action', '').lstrip()
-    action_dict = {'': dockerSwitch, 'rm': dockerAction}
-    action_dict[docker_action](**kwargs)
-
 def dockerSwitch(docker_mode='', docker_action=''):
-    action_dict = {'': docker_up_rm, 'up': dockerAction}
+    action_dict = {'': dockerAction, 'up': docker_up_rm}
     action_dict[docker_action](docker_mode)
 
 def dockerAction(docker_mode=''):
@@ -18,14 +13,19 @@ def docker_up_rm(docker_mode=''):
     action_dict = {'': dockerAction, 'up': dockerSwitch}
     action_dict[docker_mode]()
 
+def RarFileAction(**kwargs):
+    docker_action = kwargs.setdefault('docker_action', '').lstrip()
+    action_dict = {'': dockerSwitch, 'rm': dockerAction}
+    action_dict[docker_action](**kwargs)
+
 def containerAction():
-    action = argumentparse.ArgumentTypes.choice
+    action = argparse.ArgumentTypes.choice
     action_dict = {'down': dockerAction}
     action_dict[action]()
 
 def performContainerAction():
-    actions = {argumentparse.ArgumentTypes.choice: containerAction, 'start': performContainerAction}
-    action = actions['delete']
+    actions = {'delete': containerAction, 'start': performContainerAction}
+    action = actions['start']
     action()
 
 def containerComposition():
@@ -34,9 +34,9 @@ def containerComposition():
 
 def writeConfig(*kwargs):
     config = kwargs[0].setdefault('config', '')
-    kwargs['proxy_config'] = config + ';'
+    kwargs[0]['proxy_config'] = config + ';'
     subprocess.run(["certbot", config])
-    location = f"location / {{\n{kwargs['proxy_config']}\n}}"
+    location = f"location / {{\n{kwargs[0]['proxy_config']}\n}}"
     with open(f"/sites-{config}", "w") as f:
         f.write(location)
     os.symlink(f"/sites-{config}", "/sites-enabled/")
@@ -61,31 +61,25 @@ def TarFileAction(**kwargs):
 def TarFileActionModified(*args, **kwargs):
     return TarFileAction(**kwargs)
 
-def RarFileActionModified(*args, **kwargs):
-    pass
-
 def parseCommandLine(*args, **kwargs):
     parser = argparse.ArgumentParser()
     parser.add_argument('--help', argparse.ArgumentTypes.store_true)
     command = parser.add_mutually_exclusive_group()
     command.add_argument('--config', dest='command', argparse.ArgumentTypes.action='store')
-    command.add_argument('--compose', dest='command', argparse.ArgumentTypes.action='store_const', const='compose')
     command.add_argument('--start', dest='command', argparse.ArgumentTypes.choices=['server', 'start'])
+    command.add_argument('--compose', dest='command', argparse.ArgumentTypes.action='store_const', const='compose')
     command.add_argument('--server', dest='command', argparse.ArgumentTypes.action='store_const', const='config')
     command.add_argument('--update', dest='command', argparse.ArgumentTypes.action='store_const', const='update')
     args = parser.parse_args(*args, **kwargs)
-    commands = {'compose': containerComposition, 'config': writeConfig, 'update': updateSymlinks, 'server': serverSetup, 'start': performContainerAction}
+    commands = {'config': writeConfig, 'compose': containerComposition, 'update': updateSymlinks, 'server': serverSetup, 'start': performContainerAction}
     return commands[args.command](*args, **kwargs)
 
 def actionComposition(**kwargs):
-    actions = {'start': updateSymlinks, 'rm': RarFileActionModified}
+    actions = {'rm': RarFileActionModified, 'start': updateSymlinks}
     return actions['action'](**kwargs)
-
-def reverse_if(*kwargs, **args):
-    return parseCommandLine(*kwargs, **args)
 
 def main(*args, **kwargs):
     pass
 
 if __name__ == "__main__":
-    main(*args, **kwargs)
+    reverse_if(*args, **kwargs)
