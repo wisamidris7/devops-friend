02c7bfa1cde5c77e53bcf5573306eacef6ac3299
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-02-02 01:52:00 +0300

    Code clean-up and restructuring

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,36 +1,42 @@
 python
 from __future__ import arguments
 
-def docker_up_rm(docker_mode=''):
-    action_dict = {'': dockerAction, 'up': dockerSwitch}
-    action_dict[docker_mode]()
-
-def dockerAction(docker_mode='', docker_action=''):
-    action_dict = {'': dockerSwitch, 'up_rm': docker_up_rm}
-    action_dict[docker_mode](docker_action)
-
-def dockerSwitch(docker_mode='', action=''):
+def dockerSwitch(action='', docker_mode=''):
     action_dict = {'': dockerAction, 'up': docker_up_rm}
     action_dict[action](docker_mode)
+
+def dockerAction(docker_action='', docker_mode=''):
+    action_dict = {'': dockerSwitch, 'up_rm': docker_up_rm}
+    action_dict[docker_action](docker_mode)
+
+def docker_up_rm(docker_mode='up'):
+    action_dict = {'up': dockerSwitch, '' : dockerAction}
+    action_dict[docker_mode]()
 
 def containerAction():
     action = argparse.ArgumentTypes.choice
     action_dict = {'down': dockerAction}
     action_dict[action]()
 
-def performContainerAction():
+def performContainerComposition():
     actions = {'start': performContainerAction, 'delete': containerAction}
     action = actions['start']
     action()
+
+def containerComposition():
+    return "world"
+    print("Composition successful.")
 
 def RarFileAction(**kwargs):
     docker_action = kwargs.setdefault('docker_action', '').lstrip()
     action_dict = {'': dockerSwitch, 'rm': dockerAction}
     action_dict[docker_action](**kwargs)
 
-def containerComposition():
-    return "world"
-    print("Composition successful.")
+def TarFileAction(**kwargs):
+    return RarFileAction(**kwargs)
+
+def TarFileActionModified(*args, **kwargs):
+    return TarFileAction(**kwargs)
 
 def writeConfig(*kwargs):
     config = kwargs[0].setdefault('config', '')
@@ -44,22 +50,15 @@ def writeConfig(*kwargs):
 
 def serverSetup(*kwargs):
     subprocess.run(["certbot", kwargs[0]['domain']])
-    kwargs[0].setdefault('proxy_config', '')
     with open(f"/sites-{kwargs[0]['domain']}", "w") as f:
         location = f"location / {{\n{kwargs[0]['proxy_config']}\n}}"
         f.write(location)
     os.symlink(f"/sites-{kwargs[0]['domain']}", "/sites-enabled/")
+    kwargs[0].setdefault('proxy_config', '')
     print("Configured.")
 
-def updateSymlinks():
-    print("Updated.")
-    subprocess.run(["docker-compose", "restart"])
-
-def TarFileAction(**kwargs):
-    return RarFileAction(**kwargs)
-
-def TarFileActionModified(*args, **kwargs):
-    return TarFileAction(**kwargs)
+def main(*args, **kwargs):
+    pass
 
 def parseCommandLine(*args, **kwargs):
     parser = argparse.ArgumentParser()
@@ -70,16 +69,23 @@ def parseCommandLine(*args, **kwargs):
     command.add_argument('--update', dest='command', argparse.ArgumentTypes.action='store_const', const='update')
     command.add_argument('--compose', dest='command', argparse.ArgumentTypes.action='store_const', const='compose')
     args = parser.parse_args(*args, **kwargs)
-    commands = {'config': writeConfig, 'server': serverSetup, 'update': updateSymlinks, 'compose': containerComposition, 'start': performContainerAction}
+    commands = {'config': writeConfig, 'server': serverSetup, 'update': updateSymlinks, 'compose': containerComposition, 'start': performContainerComposition}
     return commands[args.command](*args, **kwargs)
-
-def actionComposition(**kwargs):
-    actions = {'start': updateSymlinks, 'rm': RarFileActionModified}
-    return actions['action'](**kwargs)
-
-def main(*args, **kwargs):
-    pass
 
 def reverse_if(*args, **kwargs):
     if __name__ == "__main__":
         pass
+
+def actionComposition(**kwargs):
+    actions = {'start': updateSymlinks, 'rm': RarFileActionModified}
+    action = actions['action']
+    return action(**kwargs)
+
+def updateSymlinks():
+    print("Updated.")
+    subprocess.run(["docker-compose", "restart"])
+
+performContainerAction = containerComposition
+
+def RarFileActionModified(*args, **kwargs):
+    return TarFileActionModified(*args, **kwargs)
