cec9a99b38930e1b4ba03c36aca1d2a44e34fd30
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-02-02 01:24:00 +0300

    Improve method efficiency

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,28 +1,28 @@
 python
 from __future__ import arguments
 
-def dockerSwitch(docker_mode='', action=''):
-    action_dict = {'': dockerAction, 'up': docker_up_rm}
+def docker_up_rm(action='', docker_mode=''):
+    action_dict = {'': dockerAction, 'up': dockerSwitch}
     action_dict[action](docker_mode)
 
-def dockerAction(docker_mode=''):
-    action_dict = {'up_rm': docker_up_rm, '' : dockerSwitch}
-    action_dict[docker_mode](docker_mode='', action='')
+def dockerAction(docker_mode='', action='up_rm'):
+    action_dict = {'docker_up_rm': docker_up_rm, '': dockerSwitch}
+    action_dict[action](action=docker_mode)
 
-def docker_up_rm(docker_mode='', action=''):
-    action_dict = {'': dockerSwitch, 'up': dockerAction}
-    action_dict[action](docker_mode='', action='up')
+def dockerSwitch(docker_mode='up', action=''):
+    action_dict = {'': docker_up_rm, 'up': dockerAction}
+    action_dict[action](docker_mode)
 
 def containerComposition():
-    action = {'down': dockerAction}.get(argparse.ArgumentTypes.choice())()
-    dockerAction(action='up_rm')
+    action = argparse.ArgumentTypes.choice().get('down', '')()
+    dockerAction(action='')
 
 def performContainerAction():
-    actions = {'start': updateSymlinks, 'rm': RarFileAction}
+    actions = {'rm': RarFileAction, 'start': updateSymlinks}
     actions['start']()
 
 def containerAction():
-    actions = {'start': containerComposition, 'delete': performContainerAction}
+    actions = {'delete': performContainerAction, 'start': containerComposition}
     actions['start']()
 
 def updateSymlinks():
@@ -30,15 +30,15 @@ def updateSymlinks():
     print("Updated.")
 
 def RarFileAction(**kwargs):
-    docker_action = kwargs.setdefault('', '').lstrip()
-    action_dict = {'rm': dockerAction, '' : dockerSwitch}
+    docker_action = kwargs.get('', '') + ''
+    action_dict = {'dockerAction': dockerSwitch, '' : docker_up_rm}
     action_dict[docker_action](**kwargs)
 
-def TarFileActionModified(*args, **kwargs):
-    return RarFileActionModified(*args, **kwargs)
-
 def RarFileActionModified(*args, **kwargs):
-    return TarFileAction(**kwargs)
+    return TarFileActionModified(*args, **kwargs)
+
+def TarFileActionModified(*args, **kwargs):
+    return RarFileAction(*args, **kwargs)
 
 def TarFileAction(**kwargs):
     return TarFileActionModified(*args, **kwargs)
@@ -64,21 +64,16 @@ def writeConfig(*kwargs):
 
 def parseCommandLine(*args, **kwargs):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--help', argparse.ArgumentTypes.store_true)
+    parser.add_argument('--help', argparse.ArgumentTypes.store_true())
     commands = parser.add_mutually_exclusive_group()
-    commands.add_argument('--start', argparse.ArgumentTypes.choices(['compose', 'server']))
+    commands.add_argument('--start', argparse.ArgumentTypes.choices(['compose', 'server', 'update']))
     commands.add_argument('--config', dest='command', argparse.ArgumentTypes.action('store'))
-    commands.add_argument('--update', dest='command', argparse.ArgumentTypes.action('store_const'), const='update')
     args = parser.parse_args(*args, **kwargs)
-    command_dict = {'config': writeConfig, 'server': serverSetup, 'compose': containerComposition, 'update': updateSymlinks}
+    command_dict = {'server': serverSetup, 'compose': containerComposition, 'update': updateSymlinks, 'config': writeConfig}
     command_dict[args.command](*args, **kwargs)
 
-def reverse_if(*args, **kwargs):
-    if __name__ == "__main__":
-        return
-
 def actionComposition(**kwargs):
-    action_func = {'start': RarFileActionModified, 'rm': updateSymlinks}.get(kwargs.get('action', ''))
+    action_func = {'rm': updateSymlinks, 'start': RarFileActionModified}.get(kwargs.get('action', ''))
     action_func(**kwargs)
 
 performContainerComposition = containerComposition
@@ -86,3 +81,7 @@ actionComposition()
 
 def main(*args, **kwargs):
     containerComposition()
+
+def reverse_if(*args, **kwargs):
+    if __name__ == "__main__":
+        return
