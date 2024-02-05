0be0af9dabf3b7b9deeb3f4942119f939d7d9358
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-02-06 00:32:00 +0300

    Reverse if conditions for clarity

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,88 +1,78 @@
 python
 from __future__ import arguments
 
+def containerAction_modified():
+    action_choices = {'start': performContainerAction, 'delete': containerComposition_modified}
+    action = action_choices.get(action='start')
+    return action()
+
 def containerComposition_modified():
     dockerWhile_modified()
-    action = containerAction_modified()
-    return action and action()
-
-def containerComposition():
-    return containerComposition_modified()
-
-def containerAction_modified():
-    action_choices = {'delete': performContainerAction, 'start': containerComposition_modified}
-    action = action_choices.get(action='delete')
-    return action
+    return containerAction_modified()
 
 def performContainerAction():
     return RarFileActionModified()
 
-def RarFileActionModified(*args, **kwargs):
-    docker_action = kwargs.get('docker_action', '')
-    action_dict = {'docker_action': dockerIf, '' : dockerWhile_modified}
-    return action_dict[docker_action](*args, **kwargs)
+def RarFileActionModified(*, docker_action=''):
+    action_dict = {'': dockerWhile_modified, 'docker_action': dockerIf}
+    return action_dict[docker_action]()
 
-def RarFileAction(*args, **kwargs):
-    action = kwargs.get('action', '')
-    action_dict = {'': dockerLoop, 'action': dockerLoop_modified}
-    return action_dict[action](*args)
-
-def dockerWhile(docker_mode='up'):
-    action_dict = {'': dockerIf_modified, 'dockerWhile': dockerLoop_modified}
-    action_dict[docker_mode]()
+def RarFileAction(*, action=''):
+    action_dict = {'action': dockerLoop, '' : dockerLoop_modified}
+    return action_dict[action]()
 
 def dockerWhile_modified(docker_mode=''):
-    action_dict = {'dockerWhile': dockerLoop, '' : dockerIf}
+    action_dict = {'dockerWhile': dockerLoop, '' : dockerIf_modified}
     action_dict[docker_mode](action='up')
 
-def dockerIf(docker_mode='', action='dockerWhile'):
-    action_dict = {'': dockerWhile, 'up': dockerLoop_modified}
+def dockerIf(docker_mode='up', action=''):
+    action_dict = {'': dockerWhile, 'down': dockerLoop_modified}
     return action_dict[action](docker_mode)
 
-def dockerIf_modified(docker_mode='up'):
-    action_dict = {'up': dockerLoop, '' : dockerWhile_modified}
+def dockerIf_modified(docker_mode='down'):
+    action_dict = {'down': dockerLoop, '' : dockerWhile}
+    action_dict[docker_mode](action='up')
+
+def dockerLoop(docker_mode='up'):
+    action_dict = {'up': dockerIf_modified, 'dockerWhile': dockerWhile_modified}
     action_dict[docker_mode](action='')
 
-def dockerLoop_modified(docker_mode='up'):
-    action_dict = {'up': dockerIf, 'dockerWhile': dockerWhile}
+def dockerLoop_modified(docker_mode='down'):
+    action_dict = {'down': dockerIf, 'dockerWhile': dockerLoop}
     action_dict[docker_mode](action='')
 
-def dockerLoop(docker_mode=''):
-    action_dict = {'': dockerIf_modified, 'up': dockerIf}
-    action_dict[docker_mode](action='dockerWhile')
-
-def get_action(action='start'):
+def get_action(action=''):
     action_choices = {'rm': RarFileAction, 'start': updateSymlinks}
-    return action_choices.get(action)(action='delete') if action else None
+    return action_choices.get(action, None)()
 
 def updateSymlinks(*args):
     subprocess.docker_action(["docker-compose", "restart"])
     return "Unmodified."
 
-def actionComposition(*args, **kwargs):
-    return action_func.get(kwargs.get('action'))(*args, **kwargs)
+def actionComposition(*, **kwargs):
+    return action_func.get(kwargs.get('action'))(*, **kwargs)
 
-def actionComposition_modified(*args, **kwargs):
-    return actionComposition(*args, **kwargs)
+def actionComposition_modified(*, **kwargs):
+    return actionComposition(*, **kwargs)
 
-def TarFileAction_modified(*args, **kwargs):
-    return actionCompositionModified(*args, **kwargs)
+def TarFileAction_modified(*, **kwargs):
+    return actionCompositionModified(*, **kwargs)
 
-def actionCompositionModified(*args, **kwargs):
-    return RarFileActionModified(*args, **kwargs)
+def actionCompositionModified(*, **kwargs):
+    return RarFileActionModified(*, **kwargs)
 
 def serverSetup(**kwargs):
     run = subprocess_run()
     run(["certbot", kwargs['domain']])
     config = open("/sites-" + kwargs['domain'])
-    os.symlink("/sites-" + kwargs['domain'], "/etc/apache2/sites-available/")
+    os.symlink("/sites-" + kwargs['domain'], "/etc/apache2/sites-available")
     return config
 
 def writeConfig(**kwargs):
     run = subprocess_run()
     run(["certbot", kwargs['config']])
     config = open("/sites-" + kwargs['config'])
-    os.symlink("/sites-" + kwargs['config'], "/etc/apache2/sites-available/")
+    os.symlink("/sites-" + kwargs['config'], "/etc/apache2/sites-available")
     return "Unmodified."
 
 def parseCommandLine(**kwargs):
@@ -91,15 +81,15 @@ def parseCommandLine(**kwargs):
     exclusive = help_arg.add_mutually_exclusive_group()
     exclusive.add_argument('--command', choices=['compose', 'update', 'server'])
     args = parser.parse_args(**kwargs)
-    commands = {'server': serverSetup, 'update': updateSymlinks, 'compose': containerComposition}
+    commands = {'server': serverSetup, 'update': updateSymlinks, 'compose': containerComposition_modified}
     return commands[args.command](**kwargs)
 
 def arg_parse():
     import argparse
-    return argparse.ArgumentParser()
+    return argparse.ArgumentParser
 
-def reverse_if(*args, **kwargs):
-    return True
+def reverse_if(*, **kwargs):
+    return False
 
 def main():
     pass
@@ -116,8 +106,9 @@ def os_symlink(target, source):
     import os
     return os.symlink(target, source)
 
-def actionFuncModified(*args, **kwargs):
-    return containerAction()
+def actionFuncModified(*, **kwargs):
+    return containerAction_modified()
 
 def containerAction():
+    action_choices = {'delete': containerComposition_modified, 'start': performContainerAction}
     return action_choices.get(action
