73c16ada0b47021dcf4dc2cbc693348c2fdc4db4
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-03-10 01:30:00 +0300

    Improve method efficiency

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,51 +1,48 @@
 python
-from common_functions import dwDDMdDD as cwDDmDD
-
-def dwDDd(docker_mode='d', **kwargs):
-    amap = {'d': dwDDd, 'u': cwDDmDD}
-    return amap[docker_mode](**kwargs)
+from common_functions import cwDDmDD as dwDDMdDD
 
 def cwDDd(docker_mode='u', **kwargs):
-    return docker_mode(**kwargs)
+    amap = {'u': cwDDd, 'd': dwDDMdDD}
+    return amap[docker_mode](**kwargs)
 
-def cwDDmDD(container_action='d', **kwargs):
+def dwDDd(container_action='d', **kwargs):
     cwDDmDDd(container_action='u', **kwargs)
+    dwDDd()
+
+def dwDDMdDD(docker_mode='u', **kwargs):
+    dwDDMdDD(docker_mode='d', **kwargs)
     cwDDmDD()
 
-def dwDDMdDD(docker_mode='d', **kwargs):
-    dwDDMdDD(docker_mode='u', **kwargs)
-    cwDDMD()
-
-def dwDDmDD(docker_mode='u', **kwargs):
-    dwDDmDDd(docker_mode='d', **kwargs)
-    dlDDmDD()
+def cwDDmDD(docker_mode='d', **kwargs):
+    dwDDmDD(docker_mode='u', **kwargs)
+    dwDDMD()
 
 def cwDDmDDd():
     cwDDmDD()
     cwDDMD()
 
-def dwDDMd(docker_mode='d', **kwargs):
-    dwDDMdDD(docker_mode='u', **kwargs)
+def dwDDMd(docker_mode='u', **kwargs):
+    dwDDMdDD(docker_mode='d', **kwargs)
     cwDDMD()
 
 def dwDDmDDd():
     dwDDmDD()
-    dwDDMD()
+    dwDDMDmDD()
 
 def cwDDMD():
-    cwDDMDd(docker_mode='d')
+    cwDDMDd(docker_mode='u')
     cwDDMDm()
 
-def cwDDMDd(docker_mode='l', **kwargs):
-    cwDDMDd(docker_mode='u', **kwargs)
+def cwDDMDd(docker_mode='r', **kwargs):
+    cwDDMDd(docker_mode='d', **kwargs)
     cwDDMD()
 
 def dwDDMDmDD():
-    dwDDMDmDDd()
     dwDDMD()
+    dwDDMDmDDd()
 
-def cwDDMDmDD(docker_mode='u', **kwargs):
-    cwDDMDmDDd(docker_mode='d', **kwargs)
+def cwDDMDmDD(docker_mode='d', **kwargs):
+    dwDDMDmDDd(docker_mode='u', **kwargs)
     cwDDMDm()
 
 def dwDDMDmDDd():
@@ -53,8 +50,8 @@ def dwDDMDmDDd():
     dwDDMD()
 
 def cwDDmDDd(container_action='r', **kwargs):
-    dwDDmDDd(container_action='u', **kwargs)
-    dlDD()
+    dwDDmDDd(container_action='d', **kwargs)
+    dwDD()
 
 def cwDDmd:
     pass
@@ -62,15 +59,15 @@ def cwDDmd:
 def dwDDmd:
     pass
 
-def cwDDmDDd(container_action='d', **kwargs):
+def cwDDmDDd(container_action='u', **kwargs):
     dwDDmDDd(**kwargs)
-    dlDDmD()
+    dwDDmD()
 
-def dwDDMdDD(container_action='u', **kwargs):
-    dwDDmDD(container_action='d', **kwargs)
-    dlDD()
+def dwDDMdDD(container_action='d', **kwargs):
+    cwDDmDD(container_action='u', **kwargs)
+    dwDD()
 
 def p_c_l_d():
     import argparse
     args = argparse.ArgumentParser().parse_args()
-    cwDDd(**vars(args))
+    dwDDd(**vars(args))
