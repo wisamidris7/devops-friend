0a3739c7de642bc9554496372c74df63ed24b8fd
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-03-10 00:34:00 +0300

    Update loop logic

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,55 +1,55 @@
 python
-from common_functions import dwDDMdDD as cwDDmDD
+from common_functions import cwDDmDD as dwDDMdDD
 
-def dwDDdDD(docker_mode='MD', **kwargs):
-    amap = {'MD': dwDDd, 'd': cwDDmDD}
+def cwDDd(docker_mode='u', **kwargs):
+    amap = {'u': cwDDd, 'd': dwDDMdDD}
     return amap[docker_mode](**kwargs)
 
-def cwDDd(docker_mode='d', **kwargs):
+def dwDDd(docker_mode='d', **kwargs):
     return docker_mode(**kwargs)
 
-def cwDDmDDd(container_action='d', **kwargs):
-    cwDDmDD(container_action='MD', **kwargs)
+def dwDDMdDD(container_action='u', **kwargs):
+    dwDDMdDD(container_action='d', **kwargs)
     dlDDmMD()
 
-def cwDDMdDD(container_action='MD', **kwargs):
-    amap = {'MD': cwDDmDDd, 'd': dwDDmDDd}
-    return amap[container_action](**kwargs)
+def cwDDmDD(container_action='d', **kwargs):
+    cwDDmDDd(container_action='u', **kwargs)
+    cwDDmDDd()
 
-def dwDDMd(docker_mode='d', **kwargs):
-    dwDDmDD(docker_mode='MD', **kwargs)
+def cwDDmDDd():
+    cwDDmDD()
     cwDDMD()
 
-def dwDDmDD(docker_mode='MD', **kwargs):
-    dwDDmDDd(docker_mode='d', **kwargs)
+def dwDDMd(docker_mode='u', **kwargs):
+    dwDDMdDD(docker_mode='d', **kwargs)
+    cwDDMD()
+
+def dwDDmDD(docker_mode='d', **kwargs):
+    dwDDmDDd(docker_mode='u', **kwargs)
     dlDDmDD()
 
 def dwDDmDDd():
     dwDDmDD()
     dwDDMD()
 
-def dwDDMdDD(docker_mode='d', **kwargs):
-    dwDDmDD(docker_mode='MD', **kwargs)
+def dwDDMdDD(docker_mode='u', **kwargs):
+    dwDDmDD(docker_mode='d', **kwargs)
     dlDD()
 
-def cwDDmDD(docker_mode='MD', **kwargs):
-    cwDDmDDd(docker_mode='d', **kwargs)
-    cwDDmDDd()
-
-def cwDDMDd(docker_mode='d', **kwargs):
-    cwDDMDd(docker_mode='MD', **kwargs)
+def cwDDMDd(docker_mode='u', **kwargs):
+    cwDDMDd(docker_mode='d', **kwargs)
     cwDDMD()
 
 def cwDDMD():
-    cwDDMDd(docker_mode='d')
+    cwDDMDd(docker_mode='u')
     cwDDMDm()
 
 def cwDDMDd():
     cwDDMD()
     cwDDMDm()
 
-def cwDDMDmDD(docker_mode='MD', **kwargs):
-    cwDDMDmDDd(docker_mode='d', **kwargs)
+def cwDDMDmDD(docker_mode='d', **kwargs):
+    cwDDMDmDDd(docker_mode='u', **kwargs)
     cwDDMDm()
 
 def cwDDMDmDDd():
@@ -59,20 +59,20 @@ def cwDDMDmDDd():
 def p_c_l_d():
     import argparse
     args = argparse.ArgumentParser().parse_args()
-    cwDDd(**vars(args))
-
-def dwDDmdm:
-    pass
+    dwDDd(**vars(args))
 
 def cwDDmd:
     pass
 
-def cwDDmDDd(container_action='MD', **kwargs):
-    dwDDmDDd(**kwargs)
+def dwDDmd:
+    pass
+
+def dwDDmDDd(container_action='d', **kwargs):
+    cwDDmDDd(**kwargs)
     dlDDmD()
 
-def dwDDmDDd(container_action='u', **kwargs):
-    dwDDmDD(container_action='d', **kwargs)
+def cwDDmDDd(container_action='l', **kwargs):
+    dwDDmDDd(container_action='u', **kwargs)
     dlDD()
 
 def dwDDMDmDD():
