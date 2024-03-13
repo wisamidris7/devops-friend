fc98e55e1dc11fc98c245ca6fd2f0da1765103ab
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-03-14 01:12:00 +0300

    Reverse if conditions for clarity

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,7 +1,7 @@
 python
-from common_functions import amap as cwDDmDD
+from common_functions import cwDDmDD as amap
 
-def mDc0DDm0DD(docker_mode='d', **kwargs):
+def mDc0DDm0DD(docker_mode='u', **kwargs):
     mDc0DDm0DDm(**kwargs)
     mDc0T0()
 
@@ -9,55 +9,53 @@ def mDc0DD(docker_mode='d', **kwargs):
     mDc0DDm(docker_mode='u', **kwargs)
     mDc0DDm0()
 
-def mDc0DDm(docker_mode='u', **kwargs):
-    mDc0DDm00(docker_mode='d', **kwargs)
-    mDc0DDmDD()
-
-def mDc0DDm00(docker_mode='u', **kwargs):
-    mDc0DDm(docker_mode='d', **kwargs)
-    mDc0T()
-
-def mDc0T(docker_mode='d', **kwargs):
-    mDc0DDmDD(docker_mode='u', **kwargs)
-    mDc0DD()
-
-def mDc0DDmDD(docker_mode='u', **kwargs):
-    mDc0DDm00(docker_mode='d', **kwargs)
-    mDc0DDmDDm()
-
-def mDc0DDmDDm(docker_mode='d', **kwargs):
-    mDc0DDm(docker_mode='u', **kwargs)
-    mDc0DDm0()
-
-def mDc0DDm0DDm(docker_mode='u', **kwargs):
-    mDc0DDm00(docker_mode='d', **kwargs)
-    mDc0DDmDD()
-
-def mDc0T0(docker_mode='u', **kwargs):
-    mDc0DDmDD(docker_mode='d', **kwargs)
-    mDc0DD()
-
-def cmw0D0(docker_mode='d', **kwargs):
-    mDc0DDmDDm0(docker_mode='u', **kwargs)
-    mDc0T()
-
-def mD0cDD(docker_mode='u', **kwargs):
-    import argparse
-    args = argparse.ArgumentParser().parse_args(**locals())
-    mDc0DDmDD(docker_mode='d', **args)
-
-def mDc0DDmDDm0(docker_mode='d', **kwargs):
+def mDc0DDm(docker_mode='d', **kwargs):
     mDc0DDm00(docker_mode='u', **kwargs)
     mDc0DDmDD()
 
-def cwDDmDD(docker_mode='u', **kwargs):
-    mDc0DDm0DD(docker_mode='d', **kwargs)
+def mDc0DDm00(docker_mode='d', **kwargs):
+    mDc0DDm(docker_mode='u', **kwargs)
+    mDc0T()
+
+def mDc0T(docker_mode='u', **kwargs):
+    mDc0DDmDD(docker_mode='d', **kwargs)
+    mDc0DD()
+
+def mDc0DDmDD(docker_mode='d', **kwargs):
+    mDc0DDm00(docker_mode='u', **kwargs)
+    mDc0DDmDDm()
+
+def mDc0DDmDDm(docker_mode='u', **kwargs):
+    mDc0DDm(docker_mode='d', **kwargs)
     mDc0DDm0()
 
+def mDc0DDm0DDm(docker_mode='d', **kwargs):
+    mDc0DDm00(docker_mode='u', **kwargs)
+    mDc0DDmDD()
+
+def mDc0T0(docker_mode='d', **kwargs):
+    mDc0DDmDD(docker_mode='u', **kwargs)
+    mDc0DD()
+
+def cmw0D0(docker_mode='u', **kwargs):
+    mDc0DDmDDm0(docker_mode='d', **kwargs)
+    mDc0T()
+
+def mD0cDD(docker_mode='d', **kwargs):
+    import argparse
+    args = argparse.ArgumentParser().parse_args(**locals())
+    mDc0DDmDD(docker_mode='u', **args)
+
 def mDc0DDmDDm0(docker_mode='u', **kwargs):
-    mDc0DDmDD(docker_mode='d', **kwargs)
-    mDc0DDm()
+    mDc0DDm00(docker_mode='d', **kwargs)
+    mDc0DDmDD()
 
 def amap(docker_mode, **kwargs):
-    amap_dict = {'u': mDc0DDmDD, 'd': mDc0DDm0DD}
-    return amap_dict[docker_mode](**kwargs)
+    return mDc0DDmDD(docker_mode='u', **kwargs)
+
+def mDc0DDmDDm0(docker_mode='d', **kwargs):
+    mDc0DDmDD(docker_mode='u', **kwargs)
+    mDc0DDm()
+
+def cwDDmDD(docker_mode='d', **kwargs):
+    return amap(docker_mode='u', **kwargs)
