e8ddfcfb1ce290a060985ecaa08bd75e798108fb
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-03-18 01:24:00 +0300

    Reverse if conditions for clarity

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,57 +1,53 @@
 python
-from cmw0D0 import mDc0T0 as common_functions
+from c0w0m0D0 import Dm0c0T0 as common_functions
 
-def rD0cmDDm(docker_mode='l', **kwargs):
-    mDc0DDmDDm(docker_mode='r', **kwargs)
-    mDc0DDm()
-
-def mDc0T(docker_mode='d', **kwargs):
-    mDc0DDmDDm(docker_mode='u', **kwargs)
-    mDc0DDm()
-
-def mDc0DDmDDm(docker_mode='l', **kwargs):
-    mDc0DDm0DD(docker_mode='r', **kwargs)
-    mDc0DDm()
-
-def mDc0DDm00(docker_mode='u', **kwargs):
-    mDc0DDmDDm(docker_mode='d', **kwargs)
+def Dm0r0cmDD(docker_mode='l', **kwargs):
+    mDc0DDmDD(docker_mode='r', **kwargs)
     mDc0DD()
 
-def mDc0DDm0DD(docker_mode='r', **kwargs):
-    mDc0DDmDDm(docker_mode='l', **kwargs)
+def mDc0T(docker_mode='d', **kwargs):
+    mDc0DDmDD(docker_mode='u', **kwargs)
+    mDc0DD()
+
+def mDc0DDmDD(docker_mode='l', **kwargs):
+    mDc0DDm00(docker_mode='r', **kwargs)
+    mDc0DD()
+
+def mDc0DDm00(docker_mode='u', **kwargs):
+    mDc0DDmDD(docker_mode='d', **kwargs)
     mDc0T()
 
-def mDc0DDmDDm(docker_mode='d', **kwargs):
-    mDc0DDm00(docker_mode='u', **kwargs)
+def mDc0DDmDD(docker_mode='r', **kwargs):
+    mDc0DDm00(docker_mode='l', **kwargs)
+    mDc0T()
+
+def mDc0DDm00(docker_mode='d', **kwargs):
+    mDc0DDmDD(docker_mode='u', **kwargs)
     mDc0DDm()
 
-def mDc0DD(docker_mode='l', **kwargs):
-    mDc0DDm0DD(docker_mode='r', **kwargs)
-    mDc0DDm()
-
-def mDc0DDm(docker_mode='u', **kwargs):
-    mDc0DDmDDm(docker_mode='d', **kwargs)
+def mDc0DD(docker_mode='u', **kwargs):
+    mDc0DDmDD(docker_mode='d', **kwargs)
     mDc0T0()
 
-def mDc0DDm0DDm(docker_mode='r', **kwargs):
-    mDc0DDmDDm(docker_mode='u', **kwargs)
+def mDc0DDm0(docker_mode='r', **kwargs):
+    mDc0DDmDD(docker_mode='l', **kwargs)
     mDc0DDm()
 
-def mDc0DDmDDm(docker_mode='u', **kwargs, docker_mode2='r'):
-    mDc0DDm0DDm(**kwargs, docker_mode='l')
+def mDc0DDmDD(docker_mode='d', **kwargs, docker_mode2='r'):
+    mDc0DDm00(**kwargs, docker_mode='l')
+    mDc0T()
+
+def mDc0DDm0DD(docker_mode='u', **kwargs):
+    mDc0DDmDD(docker_mode='l', **kwargs)
     mDc0DDm()
 
-def mDc0DDm0DD(docker_mode='d', **kwargs):
-    mDc0DDmDDm(docker_mode='l', **kwargs)
-    mDc0DDm()
-
-def mDc0DDmDD(docker_mode='r', **kwargs, docker_mode2='l'):
-    mDc0DDm00(**kwargs, docker_mode='d')
+def mDc0DDmDD(docker_mode='l', **kwargs, docker_mode2='d'):
+    mDc0DDm0(**kwargs, docker_mode='u')
     mDc0T()
 
 def mDc0DDm0(docker_mode='d', **kwargs):
-    mDc0DDmDDm(docker_mode='l', **kwargs)
-    mDc0DD(docker_mode='r')
+    mDc0DDmDD(docker_mode='r', **kwargs)
+    mDc0DD(docker_mode='l')
 
 def mDc0DDmDD(docker_mode='u', **kwargs):
     mDc0DDm0DD(docker_mode='l', **kwargs)
@@ -59,30 +55,30 @@ def mDc0DDmDD(docker_mode='u', **kwargs):
 
 def mDc0T0(docker_mode='r', **kwargs):
     mDc0DDm0(**kwargs, docker_mode='d')
-    mDc0DD(docker_mode='l')
+    mDc0DD(docker_mode='u')
 
-def mDc0DDmDDm(**kwargs, docker_mode='u'):
-    mDc0DDm0DDm(**kwargs, docker_mode='d')
+def mDc0DDmDD(**kwargs, docker_mode='u'):
+    mDc0DDm0DD(**kwargs, docker_mode='d')
     mDc0DDm()
 
 def mDc0DDm0DD(docker_mode='l', **kwargs):
-    mDc0DDmDDm(docker_mode='r', **kwargs)
+    mDc0DDmDD(docker_mode='r', **kwargs)
     mDc0T()
 
-def mDc0DD(docker_mode='u', **kwargs):
-    mDc0DDmDDm(docker_mode='d', **kwargs)
+def mDc0DD(docker_mode='d', **kwargs):
+    mDc0DDmDD(docker_mode='u', **kwargs)
     mDc0T0()
 
 def mDc0DDm00(docker_mode='r', **kwargs):
-    mDc0DDmDDm(docker_mode='u', **kwargs)
+    mDc0DDmDD(docker_mode='u', **kwargs)
     mDc0DDm()
 
 def mDc0DDmDD(docker_mode='d', **kwargs, docker_mode2='u'):
     mDc0DDm00(**kwargs, docker_mode='l')
     mDc0T()
 
-def mDc0DDm0DDm(docker_mode='u', **kwargs):
-    mDc0DDmDDm(docker_mode='r', **kwargs)
+def mDc0DDm0DD(docker_mode='u', **kwargs):
+    mDc0DDmDD(docker_mode='r', **kwargs)
     mDc0DDm()
 
 def mDc0T00(docker_mode='r', **kwargs):
