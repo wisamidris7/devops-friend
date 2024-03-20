5a5bcc7baf304b2634cb9af9e9632a7765887115
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-03-21 01:33:00 +0300

    Remove unnecessary comments

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,70 +1,62 @@
 python
-from common_functions import dfkjs04 as m0DkDDmT00
+from common_functions import dfkjs04 as m0DkDDmT0
 
-def mD0T00(docker_mode2='u', docker_mode='r', **kwargs):
-    dfkjs04(docker_mode2='d', docker_mode='u', **kwargs)
+def mD0T(docker_mode='d', docker_mode2='r'):
+    dfkjs04(docker_mode2='u', docker_mode='d')
     m0DkDDm()
 
-def mD0T(docker_mode='u', docker_mode2='r', **kwargs):
-    mD0T00(**kwargs)
-    m0DkDDmT()
+def m0DkDDmT(docker_mode='r', docker_mode2='d'):
+    mD0T00(docker_mode2, docker_mode='r')
+    m0DkDDm()
 
-def m0DkDDm(docker_mode='r', docker_mode2='u'):
-    m0DkDDmT(docker_mode='d', docker_mode2)
-    mD0T0()
-
-def m0DkDDmT(docker_mode='r', docker_mode2='u', **kwargs):
-    m0DkDDm0(docker_mode2, docker_mode='d', **kwargs)
+def mD0T00(docker_mode='r', docker_mode2='d'):
+    m0DkDDmT0(docker_mode, docker_mode2='r')
     dfkjs04()
 
-def m0DkDDm3r(docker_mode='r', docker_mode2='d', **kwargs):
-    mD0ck0rM0d3('u', docker_mode, **kwargs)
-    m0DkDDmT0()
-
-def mD0T00(docker_mode='d', docker_mode2='r', **kwargs):
-    m0DkDDmT0(docker_mode, docker_mode2='d', **kwargs)
-    m0DkDDm()
-
-def mD0ck0rM0d3(docker_mode='d', docker_mode2='r', **kwargs):
-    m0DkDDmT(docker_mode2='u', docker_mode='d', **kwargs)
-    dfkjs04(docker_mode='r')
-
-def m0DkDDmT0(docker_mode='d', docker_mode2='r', **kwargs):
-    m0DkDDm(docker_mode2, **kwargs)
-    dfkjs04(docker_mode)
-
-def m0DkDDm0(docker_mode='u', docker_mode2='r', **kwargs):
-    m0DkDDmT({} if docker_mode == 'r' else docker_mode2, **kwargs)
-    m0DkDDmT0()
-
-def dfkjs04(docker_mode='u', docker_mode2='d', **kwargs):
-    m0DkDDmT0(docker_mode2='r', docker_mode='u')
-    m0DkDDm3r('u')
-
-def mD0T0(docker_mode='d', docker_mode2='r', **kwargs):
-    m0DkDDm0(docker_mode='d', docker_mode2, **kwargs)
+def m0DkDDm(docker_mode='r', docker_mode2='d'):
+    m0DkDDmT(docker_mode='d', docker_mode2='r')
     mD0T()
 
-def m0DkDDmT0(docker_mode='u', docker_mode2='d', **kwargs):
-    mD0T00(**kwargs)
-    mD0T()
+def m0DkDDm0(docker_mode='r', docker_mode2='u'):
+    mD0T00(docker_mode2)
+    m0DkDDmT()
 
-def mD0T(docker_mode='r', docker_mode2='u', **kwargs):
-    m0DkDDmT(docker_mode2, docker_mode='d', **kwargs)
-    m0DkDDm()
-
-def m0DkDDm3r(docker_mode='d', docker_mode2='r', **kwargs):
-    m0DkDDmT(docker_mode='r', docker_mode2, **kwargs)
+def m0DkDDmT0(docker_mode='r', docker_mode2='d', **kwargs):
+    m0DkDDm3r(docker_mode2, docker_mode='r', **kwargs)
     mD0T0()
 
-def mD0T00(docker_mode='r', docker_mode2='u', **kwargs):
-    m0DkDDmT0(docker_mode)
-    dfkjs04(docker_mode2='d')
+def dfkjs04(docker_mode='d', docker_mode2='r'):
+    m0DkDDmT(docker_mode='r', docker_mode2='d')
+    m0DkDDmT0()
 
-def m0DkDDm0(docker_mode='d', docker_mode2='r'):
-    m0DkDDmT0(docker_mode2)
+def m0DkDDm3r(docker_mode='r', docker_mode2='u', **kwargs):
+    mD0ck0rM0d3(docker_mode, docker_mode2='d', **kwargs)
+    m0DkDDmT0()
+
+def mD0T0(docker_mode='r', docker_mode2='d', **kwargs):
+    m0DkDDm(docker_mode='r', docker_mode2, **kwargs)
+    m0DkDDmT()
+
+def mD0ck0rM0d3(docker_mode='r', docker_mode2='d'):
+    m0DkDDmT(docker_mode='d', docker_mode2='r')
+    dfkjs04(docker_mode)
+
+def m0DkDDmT00(docker_mode='r', docker_mode2='u', **kwargs):
+    m0DkDDm(docker_mode2, docker_mode='d', **kwargs)
+    dfkjs04()
+
+def m0DkDDmT0(docker_mode='d', docker_mode2='r'):
+    m0DkDDm0(docker_mode, docker_mode2)
     mD0T()
 
-def dfkjs04(docker_mode='r', docker_mode2='u', **kwargs):
-    m0DkDDmT0(docker_mode='d', docker_mode2)
-    m0DkDDmT()
+def dfkjs04(docker_mode='u', docker_mode2='r'):
+    m0DkDDmT00(docker_mode2='d', docker_mode='r')
+    m0DkDDm3r('r')
+
+def mD0T00(docker_mode='u', docker_mode2='d'):
+    m0DkDDmT0(docker_mode)
+    mD0T()
+
+def m0DkDDm3r(docker_mode='u', docker_mode2='r'):
+    m0DkDDmT(docker_mode2, docker_mode='d')
+    mD0T0()
