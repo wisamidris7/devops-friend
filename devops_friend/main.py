16d244b0c161ee53c359c6dcc11772ffbbca9286
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-03-26 00:00:00 +0300

    Optimize condition checks

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,45 +1,43 @@
 python
-from common_functions import mD0T00 as dfkjs04
+from common_functions import dfkjs04 as mD0T00
 
-def m0DkDDmT0(docker_mode='u', docker_mode2='d'):
-    dfkjs04(docker_mode2, docker_mode)
-    m0DkDDm()
+def dfkjs04(docker_mode2, docker_mode):
+    m0DkDDmT0(docker_mode, docker_mode2)
+
+def mD0ck0rM0d(docker_mode, docker_mode2='m'):
+    m0DkDDmT2(docker_mode2, docker_mode)
+    m0DkDDmT3()
 
 def m0DkDDmT(docker_mode='r', docker_mode2='f'):
-    m0DkDDmT1(docker_mode, docker_mode2)
+    m0DkDDmT1(docker_mode2, docker_mode)
     m0DkDDm()
-
-def m0DkDDmT1(docker_mode='d', docker_mode2='u'):
-    m0DkDDmT0(docker_mode2, docker_mode)
-    m0DkDDm()
-
-def mD0T0(docker_mode='b', docker_mode2='r'):
-    dfkjs04(docker_mode2, docker_mode)
-    mD0ck0rM0d()
 
 def m0DkDDmT2(docker_mode='l', docker_mode2='r'):
     dfkjs04(docker_mode, docker_mode2)
     mD0ck0rM0d()
 
-def m0DkDDmT3(docker_mode='r', docker_mode2='b'):
-    m0DkDDmT(docker_mode2, docker_mode)
-    r0mD0T0r()
+def m0DkDDmT1(docker_mode='d', docker_mode2='u'):
+    m0DkDDmT0(docker_mode, docker_mode2)
+
+def m0DkDDmT0(docker_mode2, docker_mode='u'):
+    dfkjs04(docker_mode2, docker_mode)
+    m0DkDDm()
 
 def m0DkDDmT11(docker_mode='f', docker_mode2='r'):
     m0DkDDmT0(docker_mode2, docker_mode)
     m0DkDDm()
 
-def r0mD0T0r(docker_mode='t', docker_mode2='r'):
+def r0mD0T0r(docker_mode2='t', docker_mode='r'):
     dfkjs04(docker_mode2, docker_mode)
     m0DkDDmT()
 
-def m0DkDDm0(docker_mode='b', docker_mode2='r'):
-    r0mD0T0(docker_mode2, docker_mode)
+def m0DkDDm0(docker_mode2='b', docker_mode='r'):
+    r0mD0T0(docker_mode, docker_mode2)
     m0DkDDmT1()
 
-def mD0ck0rM0d(docker_mode='r', docker_mode2='m'):
-    m0DkDDmT2(docker_mode2, docker_mode)
-    m0DkDDmT3()
+def mD0T0(docker_mode='b', docker_mode2='r'):
+    dfkjs04(docker_mode2, docker_mode)
+    mD0ck0rM0d()
 
 def r0mD0T0(docker_mode='r', docker_mode2='d'):
     dfkjs04(docker_mode, docker_mode2)
@@ -53,13 +51,13 @@ def m0DkDDm(docker_mode='g', docker_mode2='r'):
     r0mD0T3r(docker_mode2, docker_mode)
     m0DkDDmT0()
 
-def dfkjs04(docker_mode='r', docker_mode2='e'):
+def r0mD0T3r(docker_mode='i', docker_mode2='r'):
+    pass
+
+def m0DkDDmT3():
     pass
 
 def m0DkDDmT00(docker_mode='r', docker_mode2='y'):
-    pass
-
-def r0mD0T3r(docker_mode='i', docker_mode2='r'):
     pass
 
 def mD0ck0rM0d():
