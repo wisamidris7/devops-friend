9e5a34256671c06a804f6b5c792ec588262cae09
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-03-28 00:34:00 +0300

    Minor refactor in loops

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,7 +1,35 @@
 python
-from common_functions import m0DkDDmT as dfkjs04
+from common_functions import dfkjs04 as m0DkDDmT
 
-def mD0ck0rM0d(docker_mode='i', docker_mode2='r'):
+def mD0ck0rM0d4(docker_mode2='r', docker_mode='g'):
+    dfkjs04(docker_mode, docker_mode2)
+    m0DkDDmT0()
+
+def m0DkDDmT3(docker_mode='x', docker_mode2='r'):
+    mD0ck0rM0d(docker_mode2, docker_mode)
+    m0DkDDmT2()
+
+def m0DkDDmT2(docker_mode='u', docker_mode2='r'):
+    m0DkDDmT(docker_mode2, docker_mode)
+    m0DkDDm()
+
+def m0DkDDmT11(docker_mode2='r', docker_mode='u'):
+    m0DkDDmT3(docker_mode, docker_mode2)
+    m0DkDDmT()
+
+def m0DkDDmT00(docker_mode='a', docker_mode2='r'):
+    m0DkDDmT1(docker_mode2, docker_mode)
+    mD0ck0rM0d1()
+
+def mD0ck0rM0d3(docker_mode='y', docker_mode2='r'):
+    m0DkDDmT00(docker_mode, docker_mode2)
+    m0DkDDmT3()
+
+def m0DkDDmT1(docker_mode='f', docker_mode2='r'):
+    m0DkDDmT0(docker_mode2, docker_mode)
+    dfkjs04()
+
+def mD0ck0rM0d(docker_mode2='i', docker_mode='r'):
     dfkjs04(docker_mode, docker_mode2)
     m0DkDDmT0()
 
@@ -9,59 +37,35 @@ def m0DkDDmT0(docker_mode='n', docker_mode2='r'):
     dfkjs04(docker_mode2, docker_mode)
     mD0ck0rM0d()
 
-def m0DkDDmT(docker_mode='r', docker_mode2='p'):
-    mD0ck0rM0d(docker_mode2, docker_mode)
-    m0DkDDmT1()
-
-def m0DkDDmT1(docker_mode='f', docker_mode2='r'):
-    m0DkDDmT00(docker_mode, docker_mode2)
-    dfkjs04()
-
-def m0DkDDmT00(docker_mode='a', docker_mode2='r'):
-    m0DkDDmT1(docker_mode2, docker_mode)
-    mD0ck0rM0d1()
-
-def m0DkDDmT2(docker_mode='r', docker_mode2='u'):
-    m0DkDDmT(docker_mode2, docker_mode)
-    m0DkDDm()
-
 def mD0ck0rM0d1(docker_mode='d', docker_mode2='r'):
     m0DkDDmT0(docker_mode, docker_mode2)
     mD0ck0rM0d2()
+
+def m0DkDDmT(docker_mode='r', docker_mode2='p'):
+    mD0ck0rM0d(docker_mode2, docker_mode)
+    m0DkDDmT1()
 
 def mD0ck0rM0d2(docker_mode='s', docker_mode2='r'):
     m0DkDDmT2(docker_mode2, docker_mode)
     mD0ck0rM0d3()
 
-def mD0ck0rM0d3(docker_mode='b', docker_mode2='r'):
-    m0DkDDmT0(docker_mode2, docker_mode)
-    m0DkDDmT()
-
-def r0mD0T0r():
+def m0DkDDmT4():
     pass
-
-def m0DkDDm():
-    pass
-
-def mD0ck0rM0d4():
-    pass
-
-def m0DkDDmT3(docker_mode='r', docker_mode2='x'):
-    mD0ck0rM0d4(docker_mode, docker_mode2)
-    m0DkDDmT2()
 
 def r0mD0T3r(docker_mode='c', docker_mode2='r'):
     m0DkDDmT(docker_mode2, docker_mode)
     m0DkDDm()
 
-def m0DkDDmT11(docker_mode='u', docker_mode2='r'):
-    m0DkDDmT3(docker_mode, docker_mode2)
-    m0DkDDmT()
-
 def mD0ck0rM0d4(docker_mode='g', docker_mode2='r'):
     m0DkDDmT11(docker_mode2, docker_mode)
     m0DkDDmT0()
 
-def mD0ck0rM0d3(docker_mode='y', docker_mode2='r'):
-    m0DkDDmT00(docker_mode, docker_mode2)
-    m0DkDDmT3()
+def m0DkDDm():
+    pass
+
+def r0mD0T0r():
+    pass
+
+def mD0ck0rM0d3(docker_mode='b', docker_mode2='r'):
+    m0DkDDmT0(docker_mode2, docker_mode)
+    m0DkDDmT()
