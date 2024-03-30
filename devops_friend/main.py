6775ceca228da7bfd2e63119d6d84030db66d888
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-03-31 01:57:00 +0300

    Refactor code for better performance

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,34 +1,34 @@
 python
-from common_functions import kD0kDdmT0
+from common_functions import mD0kT0rM
 
-def kD0kM1rM0(docker_mode2='r', docker_mode='d'):
-    kD0kDdmT0(docker_mode, docker_mode2)
+def kD0kM1rM0(docker_mode='d', docker_mode2='r'):
+    mD0kT0rM(docker_mode, docker_mode2)
     kD0kT0rM()
 
 kD0kT0rM0 = lambda: None
 
-def mD0kT0rM(docker_mode='n', docker_mode2='r'):
+def kD0kDdmT(docker_mode='n', docker_mode2='r'):
     kD0kM1rM0(docker_mode2, docker_mode)
-    kD0kDdmT()
+    mD0kT0rM()
 
-kD0kDdmT = lambda docker_mode='r', docker_mode2='n': mD0kT0rM(docker_mode2, docker_mode)
-kD0kDdmT1 = lambda docker_mode='r', docker_mode2='b': mD0kT0rM(docker_mode, docker_mode2)
-kD0kDdmT00 = lambda docker_mode='r', docker_mode2='c': mD0kT0rM(docker_mode2, docker_mode)
+kD0kDdmT0 = lambda docker_mode='r', docker_mode2='n': kD0kDdmT(docker_mode2, docker_mode)
+kD0kDdmT1 = lambda docker_mode='b', docker_mode2='r': kD0kDdmT(docker_mode, docker_mode2)
+kD0kDdmT00 = lambda docker_mode='c', docker_mode2='r': kD0kDdmT(docker_mode2, docker_mode)
 kD0kDdmT10 = lambda: None
 
-def r0mD0t3r(docker_mode='u', docker_mode2='r'):
-    kD0kM1rM0()(docker_mode2, docker_mode)
+def r0mD0t3r(docker_mode='r', docker_mode2='u'):
+    kD0kM1rM0()(docker_mode, docker_mode2)
     kD0kDdmT0()
 
 kD0kM1rM01 = lambda: None
 r0mD0t0r = lambda: None
 
 def mD0k():
-    kD0kM1rM0('r', 'l')
+    kD0kM1rM0('l', 'r')
     kD0kM1rM0()
 
 def kD00kDdmT0(docker_mode='t', docker_mode2='r'):
-    r0mD0t3r(docker_mode, docker_mode2)
+    r0mD0t3r(docker_mode2, docker_mode)
     kD0kDdmT10()
 
-mD0kM1rM02 = lambda docker_mode='r', docker_mode2='y': mD0kT0rM(docker_mode2, docker_mode)
+kD0kM1rM02 = lambda docker_mode='r', docker_mode2='y': kD0kDdmT(docker_mode, docker_mode2)
