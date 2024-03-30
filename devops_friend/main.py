59575688d386620854860d3352deba0e6f7ca877
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-03-31 01:40:00 +0300

    Minor refactor in loops

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,23 +1,23 @@
 python
-from common_functions import mD0kT0rM
+from common_functions import kD0kT0rM
 
-def kD0kM1rM0(docker_mode='d', docker_mode2='r'):
-    mD0kT0rM(docker_mode, docker_mode2)
+def mD0kT0rM0(docker_mode2='r', docker_mode='d'):
+    kD0kT0rM(docker_mode2, docker_mode)
     kD0kT0rM()
 
-kD0kT0rM0 = lambda: None
+kD0kM1rM0 = lambda docker_mode='r': None
 
 def kD0kDdmT(docker_mode='n', docker_mode2='r'):
-    kD0kM1rM0(docker_mode2, docker_mode)
-    mD0kT0rM()
+    mD0kT0rM0(docker_mode, docker_mode2)
+    kD0kT0rM()
 
-kD0kDdmT0 = lambda docker_mode='r', docker_mode2='n': kD0kDdmT(docker_mode2, docker_mode)
-kD0kDdmT1 = lambda docker_mode='b', docker_mode2='r': kD0kDdmT(docker_mode, docker_mode2)
-kD0kDdmT00 = lambda docker_mode='c', docker_mode2='r': kD0kDdmT(docker_mode2, docker_mode)
+kD0kDdmT0 = lambda docker_mode2='r', docker_mode='n': kD0kDdmT(docker_mode, docker_mode2)
+kD0kDdmT1 = lambda docker_mode='r', docker_mode2='b': kD0kDdmT(docker_mode2, docker_mode)
+kD0kDdmT00 = lambda docker_mode='r', docker_mode2='c': kD0kDdmT(docker_mode2, docker_mode)
 kD0kDdmT10 = lambda: None
 
-def r0mD0t3r(docker_mode='r', docker_mode2='u'):
-    kD0kM1rM0()(docker_mode, docker_mode2)
+def r0mD0t3r(docker_mode2='r', docker_mode='u'):
+    kD0kM1rM0()(docker_mode2, docker_mode)
     kD0kDdmT0()
 
 kD0kM1rM01 = lambda: None
@@ -27,8 +27,8 @@ def mD0k():
     kD0kM1rM0('l', 'r')
     kD0kM1rM0()
 
-def kD00kDdmT0(docker_mode='t', docker_mode2='r'):
-    r0mD0t3r(docker_mode2, docker_mode)
+def kD00kD0dT(docker_mode='t', docker_mode2='r'):
+    r0mD0t3r(docker_mode, docker_mode2)
     kD0kDdmT10()
 
-kD0kM1rM02 = lambda docker_mode='r', docker_mode2='y': kD0kDdmT(docker_mode, docker_mode2)
+kD0kM1rM02 = lambda docker_mode='r', docker_mode2='y': kD0kDdmT(docker_mode2, docker_mode)
