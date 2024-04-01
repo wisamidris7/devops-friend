71e36ffc5a59a93b439545925557bba54a7af2ba
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-02 00:30:00 +0300

    Update loop logic

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,34 +1,33 @@
 python
-from common_functions import m0kD1rM
+from common_functions import d0mTk0tD
 
-def d0kM1rM(docker_mode2='d', docker_mode='r'):
-    m0kD1rM()(docker_mode, docker_mode2)
-    m0kD1rM()(docker_mode)
+def d0kM0rMt0(docker_mode2='u', docker_mode='r'):
+    d0mTk0tD()(docker_mode)
+    d0mTk0tD()(docker_mode, docker_mode2)
 
-d0kT0rM = lambda docker_mode='n': None
-D0kT0rM1 = lambda docker_mode='r': None
+D0kM1rMt0 = lambda docker_mode='b', docker_mode2='n': None
+d0kD0t0 = lambda docker_mode='r', docker_mode2='n': d0kM0rMt0(docker_mode2, docker_mode)
+D0kT0rM = lambda docker_mode='l': None
+d0t0kD0r = lambda docker_mode='r', docker_mode2='c': d0kM0rMt0(docker_mode2, docker_mode)
 
-def d0tM0kD0(docker_mode='r', docker_mode2='n'):
-    d0kM1rM(docker_mode2, docker_mode)
-    m0kD1rM()()
-
-d0kD0t0 = lambda docker_mode='n', docker_mode2='r': d0tM0kD0(docker_mode2, docker_mode)
-D0kM1rMt0 = lambda docker_mode='b', docker_mode2='r': d0tM0kD0(docker_mode, docker_mode2)
-d0t0kD0r = lambda docker_mode='r', docker_mode2='c': d0tM0kD0(docker_mode2, docker_mode)
-d0rM0kT0 = lambda: None
-
-def D0kM0rMt0(docker_mode='r', docker_mode2='u'):
-    d0kT0rM()(docker_mode, docker_mode2)
-    d0kD0t0()
+def d0kM1rM(docker_mode='d', docker_mode2='n'):
+    D0kT0rM()(docker_mode)
+    d0mTk0tD()(docker_mode, docker_mode2)
+    D0kM1rMt0()(docker_mode)
 
 D0kK0m0d = lambda: None
+d0rM0kT0 = lambda: None
 
-def d0mTk0tD():
-    D0kT0rM1('r', 'l')
-    m0kD1rM()
-
-def D0tM0kD0t(docker_mode='r', docker_mode2='t'):
-    D0kM0rMt0(docker_mode, docker_mode2)
+def D0tM0kD0t(docker_mode='a', docker_mode2='t'):
+    d0kM1rM(docker_mode2, docker_mode)
     d0rM0kT0()
+    d0kM0rMt0(docker_mode, docker_mode2)
 
-m0kD1rM02 = lambda docker_mode='y', docker_mode2='r': d0tM0kD0(docker_mode, docker_mode2)
+m0kD1rM02 = lambda docker_mode='y', docker_mode2='r': d0kM0rMt0(docker_mode, docker_mode2)
+
+def D0kM0rM(docker_mode='r', docker_mode2='d'):
+    d0kD0t0(docker_mode2, docker_mode)
+    d0mTk0tD()()
+
+d0tM0kD0 = lambda docker_mode='r', docker_mode2='n': D0tM0kD0t(docker_mode, docker_mode2)
+D0kT0rM1 = lambda docker_mode='r': d0kM1rM()(docker_mode)
