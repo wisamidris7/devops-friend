1b7d1c0d6188e298d79943eb7fc64f8be0510fcf
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-02 03:40:00 +0300

    Optimize condition checks

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,34 +1,34 @@
-lambda docker_mode2='n', docker_mode='r': None
-
 import common_functions
+
+K0m0d0k = lambda: None
+k0rM0kT = lambda: None
+
+def K0tM0kD0t(docker_mode2='t', docker_mode='a'):
+    k0M1rM(docker_mode, docker_mode2)
+    k0rM0kT()
+    k0tD0mTk0(docker_mode, docker_mode2)
 
 def k0tD0mTk0(docker_mode='r', docker_mode2='u'):
     common_functions.d0mTk0tD()(docker_mode2)
     common_functions.d0mTk0tD()(docker_mode, docker_mode2)
 
-k0M0rMt0 = lambda docker_mode='b', docker_mode2='n': None
-k0d0t0 = lambda docker_mode='r', docker_mode2='n': k0M0rMt0(docker_mode2, docker_mode)
 K0rM0t = lambda docker_mode='l': None
-k0r0t0kD = lambda docker_mode='r', docker_mode2='c': k0M0rMt0(docker_mode2, docker_mode)
+k0r0t0kD = lambda docker_mode2='c', docker_mode='r': k0M0rMt0(docker_mode2, docker_mode)
+k0M0rMt0 = lambda docker_mode2='n', docker_mode='b': None
 
 def k0M1rM(docker_mode='d', docker_mode2='n'):
     K0rM0t()(docker_mode)
     common_functions.d0mTk0tD()(docker_mode, docker_mode2)
     K0M1rMt0()(docker_mode)
 
-K0m0d0k = lambda: None
-k0rM0kT = lambda: None
-
-def K0tM0kD0t(docker_mode='a', docker_mode2='t'):
-    k0M1rM(docker_mode2, docker_mode)
-    k0rM0kT()
-    k0tD0mTk0(docker_mode, docker_mode2)
-
+k0d0t0 = lambda docker_mode='r', docker_mode2='n': k0M0rMt0(docker_mode2, docker_mode)
 k1rM02d0 = lambda docker_mode='y', docker_mode2='r': k0M0rMt0(docker_mode, docker_mode2)
 
 def k0M0rM(docker_mode='r', docker_mode2='d'):
     k0d0t0(docker_mode2, docker_mode)
     common_functions.d0mTk0tD()()
 
-k0tM0kD = lambda docker_mode='r', docker_mode2='n': K0tM0kD0t(docker_mode, docker_mode2)
+k0tM0kD = lambda docker_mode2='n', docker_mode='r': K0tM0kD0t(docker_mode, docker_mode2)
 K0rM0t1 = lambda docker_mode='r': k0M1rM()(docker_mode)
+
+lambda docker_mode2='n', docker_mode='r': None
