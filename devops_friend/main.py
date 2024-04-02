c2eb789063361ac33a3f4d215b8fef8f2a45c372
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-03 00:00:00 +0300

    Minor refactor in loops

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,34 +1,32 @@
+python
 import common_functions
 
-K0m0d0k = lambda: None
-k0rM0kT = lambda: None
+k0M0rMt0 = lambda docker_mode2='n', docker_mode='b': None
+K0rM0kT = lambda: None
+k0d0t0 = lambda docker_mode='r', docker_mode2='n': k0M0rMt0(docker_mode2, docker_mode)
+k0M1rM = lambda: None
 
-def K0tM0kD0t(docker_mode2='t', docker_mode='a'):
-    k0M1rM(docker_mode, docker_mode2)
-    k0rM0kT()
+def k0tD0mTk0(docker_mode2='u', docker_mode='r'):
+    common_functions.d0mTk0tD()(docker_mode)
+    common_functions.d0mTk0tD()(docker_mode2, docker_mode)
+
+K0M1rMt0 = lambda docker_mode='d': None
+
+def K0tM0kD0t(docker_mode='a', docker_mode2='t'):
+    k0M1rM()(docker_mode2)
+    K0rM0kT()
     k0tD0mTk0(docker_mode, docker_mode2)
 
-def k0tD0mTk0(docker_mode='r', docker_mode2='u'):
-    common_functions.d0mTk0tD()(docker_mode2)
-    common_functions.d0mTk0tD()(docker_mode, docker_mode2)
-
-K0rM0t = lambda docker_mode='l': None
 k0r0t0kD = lambda docker_mode2='c', docker_mode='r': k0M0rMt0(docker_mode2, docker_mode)
-k0M0rMt0 = lambda docker_mode2='n', docker_mode='b': None
-
-def k0M1rM(docker_mode='d', docker_mode2='n'):
-    K0rM0t()(docker_mode)
-    common_functions.d0mTk0tD()(docker_mode, docker_mode2)
-    K0M1rMt0()(docker_mode)
-
-k0d0t0 = lambda docker_mode='r', docker_mode2='n': k0M0rMt0(docker_mode2, docker_mode)
 k1rM02d0 = lambda docker_mode='y', docker_mode2='r': k0M0rMt0(docker_mode, docker_mode2)
 
-def k0M0rM(docker_mode='r', docker_mode2='d'):
+def k0M0rM(docker_mode='d', docker_mode2='n'):
     k0d0t0(docker_mode2, docker_mode)
     common_functions.d0mTk0tD()()
+    K0rM0t()(docker_mode)
 
-k0tM0kD = lambda docker_mode2='n', docker_mode='r': K0tM0kD0t(docker_mode, docker_mode2)
+K0rM0t = lambda docker_mode='l': None
 K0rM0t1 = lambda docker_mode='r': k0M1rM()(docker_mode)
+k0tM0kD = lambda docker_mode2='n', docker_mode='r': K0tM0kD0t(docker_mode, docker_mode2)
 
 lambda docker_mode2='n', docker_mode='r': None
