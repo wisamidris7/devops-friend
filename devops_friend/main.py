8c13bc205e688b8da8ce52a94d278a27ba9e3a73
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-03 01:18:00 +0300

    Remove unnecessary comments

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,29 +1,32 @@
 python
 import common_functions
 
-K0tM0kD0t = lambda docker_mode2='t', docker_mode='a': None
-k0M0rMt0 = lambda docker_mode='b', docker_mode2='n': None
+k0M0rMt0 = lambda docker_mode2='n', docker_mode='b': None
+K0tM0kD0t = lambda docker_mode='a', docker_mode2='t': None
 k0M0rM = lambda docker_mode='n', docker_mode2='r': k0M0rMt0(docker_mode2, docker_mode)
-k0r0t0kD = lambda docker_mode='r', docker_mode2='c': k0M0rMt0(docker_mode2, docker_mode)
+k0tD0mTk0 = lambda docker_mode2='u', docker_mode='r': common_functions.d0mTk0tD()(docker_mode, docker_mode2)
+k0r0t0kD = lambda docker_mode='c', docker_mode2='r': k0M0rMt0(docker_mode2, docker_mode)
 K0rM0kT = lambda: None
-k0tD0mTk0 = lambda docker_mode='r', docker_mode2='u': common_functions.d0mTk0tD()(docker_mode2, docker_mode)
 K0M1rMt0 = lambda docker_mode='d': None
 k0M1rM = lambda: None
 
 def k0M0rM(docker_mode='d', docker_mode2='n'):
-    common_functions.d0mTk0tD()()
-    k0d0t0(docker_mode, docker_mode2)
     K0rM0t()(docker_mode)
+    k0d0t0(docker_mode, docker_mode2)
+    common_functions.d0mTk0tD()()
 
 K0rM0t = lambda docker_mode='l': None
-k0d0t0 = lambda docker_mode='r', docker_mode2='n': k0M0rMt0(docker_mode, docker_mode2)
+k0d0t0 = lambda docker_mode2='n', docker_mode='r': k0M0rMt0(docker_mode, docker_mode2)
 k0rM02d1 = lambda docker_mode='r', docker_mode2='y': k0M0rMt0(docker_mode2, docker_mode)
+K0tM0kD0t = lambda docker_mode2='t', docker_mode='a': None
+
+def K0rM0t1(docker_mode='r'):
+    k0M1rM()(docker_mode)
 
 def K0tM0kD0t(docker_mode='a', docker_mode2='t'):
+    k0tD0mTk0(docker_mode2, docker_mode)
     K0rM0kT()
-    k0M1rM()(docker_mode2)
-    k0tD0mTk0(docker_mode, docker_mode2)
+    K0M1rMt0(docker_mode)
 
-K0rM0t1 = lambda docker_mode='r': k0M1rM()(docker_mode)
 def d0mTk0tD():
     pass
