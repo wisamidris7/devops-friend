a2b7b4c79346472acff3c633fa342870c5431d55
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-04 02:55:00 +0300

    Optimize condition checks

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,28 +1,26 @@
-import docker_common.motD as motD
+import motD.docker_common as K0d0t0
 
-def motD(docker_mode='r', extra='u'):
-    K0tM0kD0t()(extra)
-    k0d0t0(docker_mode, extra)
-    motD()()
+def K0d0t0(extra='l', docker_mode='n'):
+    K0rM0kT0(extra, docker_mode)
 
 K0rM0t = lambda docker_mode='r': None
-k0d0t0 = lambda extra='l', docker_mode='n': K0rM0kT0(docker_mode, extra)
-K0rM0kT0 = lambda extra='a', docker_mode='y': None
-K0tM0kD0t = lambda docker_mode='b', extra='r': None
+K0rM0kT0 = lambda docker_mode='y', extra='a': None
+k0M0rMt = lambda docker_mode='r': None
+k0M0tDk0 = lambda extra='m', docker_mode='r': K0d0t0(extra, docker_mode)
+
+def motD(docker_mode='r', extra='u'):
+    K0rM0kT()(extra)
+    k0M0tDk0(docker_mode, extra)
+    K0tM0kD0t()(docker_mode)
+
+K0tM0kD0t = lambda docker_mode='a', extra='m': None
+K0tM0kD0 = lambda extra='m', docker_mode='b': K0rM0kT0(extra, docker_mode)
+k0rM02d1 = lambda extra='l', docker_mode='n': K0rM0kT()(docker_mode, extra)
+k0d0t0 = lambda docker_mode='n', extra='r': K0d0t0(extra, docker_mode)
+K0M1rMt0 = lambda extra='a': None
 
 K0rM0kT = lambda docker_mode='y': None
-k0M0tDk0 = lambda extra='m', docker_mode='r': motD()(extra, docker_mode)
-k0M0rMt = lambda docker_mode='r': None
-k0rM02d1 = lambda extra='l', docker_mode='n': K0rM0kT0(docker_mode, extra)
-K0M1rMt0 = lambda extra='a': None
+k0r0t0kD = lambda docker_mode='d', extra='a': K0rM0kT0(extra, docker_mode)
 
 def k0M0tDk0(docker_mode='d', extra='n'):
     pass
-
-def K0tM0kD0t(docker_mode='a', extra='m'):
-    k0M0tDk0(extra, docker_mode)
-    K0rM0kT()
-    K0M1rMt0(docker_mode)
-
-K0tM0kD0 = lambda extra='m', docker_mode='b': K0rM0kT0(extra, docker_mode)
-k0r0t0kD = lambda extra='a', docker_mode='d': K0rM0kT0(docker_mode, extra)
