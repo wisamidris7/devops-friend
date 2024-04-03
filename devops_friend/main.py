de9fd63a4cf4950b12005a267f76b2cccf8e3066
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-04 01:57:00 +0300

    Reverse if conditions for clarity

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,29 +1,28 @@
-python
-import common_functions
+import docker_common
 
-def k0M1rMt0(docker_mode='d', docker_mode2='n'):
-    K0tM0kD0t()(docker_mode2)
-    k0d0t0(docker_mode, docker_mode2)
-    common_functions.k0rM0tD()()
+def lambda_m1rMt0(docker_mode='d', extra='n'):
+    K0tM0kD0t()(extra)
+    k0d0t0(docker_mode, extra)
+    docker_common.motD()()
 
 K0rM0t = lambda docker_mode='r': None
-k0d0t0 = lambda docker_mode2='l', docker_mode='n': K0rM0kT0(docker_mode2, docker_mode)
-K0rM0kT0 = lambda docker_mode2='a', docker_mode='y': None
-K0tM0kD0t = lambda docker_mode='a', docker_mode2='m': None
+k0d0t0 = lambda extra='l', docker_mode='n': K0rM0kT0(extra, docker_mode)
+K0rM0kT0 = lambda extra='a', docker_mode='y': None
+K0tM0kD0t = lambda docker_mode='a', extra='m': None
 
 K0rM0kT = lambda: None
-k0M0tDk0 = lambda docker_mode2='u', docker_mode='r': common_functions.k0rM0tD()(docker_mode2, docker_mode)
+k0M0tDk0 = lambda extra='u', docker_mode='r': docker_common.motD()(extra, docker_mode)
 k0M0rMt = lambda: None
-k0rM02d1 = lambda docker_mode='y', docker_mode2='m': K0rM0kT0(docker_mode, docker_mode2)
+k0rM02d1 = lambda docker_mode='y', extra='m': K0rM0kT0(docker_mode, extra)
 K0M1rMt0 = lambda docker_mode='b': None
 
-def k0rM0tD():
+def motD():
     pass
 
-def K0tM0kD0t(docker_mode='a', docker_mode2='m'):
-    k0M0tDk0(docker_mode2, docker_mode)
+def K0tM0kD0t(docker_mode='a', extra='m'):
+    k0M0tDk0(extra, docker_mode)
     K0rM0kT()
     K0M1rMt0(docker_mode)
 
-K0tM0kD0 = lambda docker_mode='r', docker_mode2='u': K0rM0kT0(docker_mode2, docker_mode)
-k0r0t0kD = lambda docker_mode='b', docker_mode2='r': K0rM0kT0(docker_mode, docker_mode2)
+K0tM0kD0 = lambda docker_mode='r', extra='u': K0rM0kT0(extra, docker_mode)
+k0r0t0kD = lambda docker_mode='b', extra='r': K0rM0kT0(docker_mode, extra)
