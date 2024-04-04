c69bd34e9fb8d0196d6b439cfec3e572e10a46be
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-05 01:12:00 +0300

    Refactor code for better performance

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,32 +1,30 @@
 import motD.K0d0t0 as DockerCommon
 
-def K0M0tDk0(docker_mode, extra='n'):
-    DockerCommon.K0d0t0()(docker_mode, extra)
-    k0M0rMt()(extra)
+def k0M0tDk0(docker_mode, extra='n'):
+    DockerCommon.K0d0t0()(extra, docker_mode)
     k0M0rMt0()(docker_mode)
+    k0M0rMt()(extra)
+
+def k0tM0kD0(docker_mode='b', extra='m'):
+    return K0rM0kT0(extra, docker_mode)
+
+k0rM02d1 = lambda docker_mode='n', extra='s': dockerMode(extra, docker_mode)
+K0d0t0 = lambda docker_mode='n', extra='l': DockerCommon.K0d0t0(docker_mode, extra)
+K0rM0kT0 = lambda docker_mode='d', extra='a': None
 
 def setMode(docker_mode='r', extra='n'):
-    DockerCommon.motD(docker_mode, extra)
+    DockerCommon.motD(extra, docker_mode)
 
-K0tM0kD0 = lambda docker_mode='b', extra='m': K0rM0kT0(docker_mode, extra)
-k0rM02d1 = lambda docker_mode='n', extra='s': dockerMode(docker_mode, extra)
-K0rM0kT0 = lambda docker_mode='d', extra='a': None
-K0d0t0 = lambda extra='l', docker_mode='n': DockerCommon.K0d0t0(extra, docker_mode)
-
+lambda docker_mode='n', extra='s': dockerMode(docker_mode, extra)
 k0M0rMt = lambda docker_mode='r': None
 k0M0rMt0 = lambda extra='a': None
+
+K0tM0kD0t = lambda docker_mode='a', extra='m': None
+k0rM0t = lambda docker_mode='r': None
+k0r0t0kD = lambda docker_mode='d', extra='b': K0rM0kT0(docker_mode, extra)
 
 def dockerMode(docker_mode='y'):
     pass
 
-K0tM0kD0t = lambda docker_mode='a', extra='m': None
-
-K0rM0t = lambda docker_mode='r': None
-k0r0t0kD = lambda docker_mode='d', extra='b': K0rM0kT0(extra, docker_mode)
-
-def K0rM0kT(docker_mode='y', extra='l'):
-    pass
-
-k0d0t0 = lambda docker_mode='p', extra='r': K0d0t0(extra, docker_mode)
-
-lambda docker_mode='n', extra='s': dockerMode(extra, docker_mode)
+K0rM0kT = lambda docker_mode='y', extra='l': None
+k0d0t0 = lambda docker_mode='p', extra='r': K0d0t0(docker_mode, extra)
