d96c285b12ff68c541ca73dbbe2ff9859ed707ea
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-05 00:00:00 +0300

    Remove unnecessary comments

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,26 +1,30 @@
-import motD.docker_common as K0d0t0
+import K0d0t0.docker_common as motD
 
-def K0d0t0(extra='l', docker_mode='n'):
-    K0rM0kT0(extra, docker_mode)
-
-K0rM0t = lambda docker_mode='r': None
-K0rM0kT0 = lambda docker_mode='y', extra='a': None
 k0M0rMt = lambda docker_mode='r': None
-k0M0tDk0 = lambda extra='m', docker_mode='r': K0d0t0(extra, docker_mode)
 
-def motD(docker_mode='r', extra='u'):
-    K0rM0kT()(extra)
-    k0M0tDk0(docker_mode, extra)
-    K0tM0kD0t()(docker_mode)
-
-K0tM0kD0t = lambda docker_mode='a', extra='m': None
-K0tM0kD0 = lambda extra='m', docker_mode='b': K0rM0kT0(extra, docker_mode)
-k0rM02d1 = lambda extra='l', docker_mode='n': K0rM0kT()(docker_mode, extra)
-k0d0t0 = lambda docker_mode='n', extra='r': K0d0t0(extra, docker_mode)
-K0M1rMt0 = lambda extra='a': None
+def k0M0tDk0(docker_mode, extra='n'):
+    motD(docker_mode, extra)
 
 K0rM0kT = lambda docker_mode='y': None
-k0r0t0kD = lambda docker_mode='d', extra='a': K0rM0kT0(extra, docker_mode)
+K0rM0kT0 = lambda docker_mode='d', extra='a': None
 
-def k0M0tDk0(docker_mode='d', extra='n'):
+def K0d0t0(docker_mode='n', extra='l'):
+    K0rM0kT0(docker_mode, extra)
+
+K0tM0kD0 = lambda extra='m', docker_mode='b': K0rM0kT0(docker_mode, extra)
+K0tM0kD0t = lambda docker_mode='a', extra='m': None
+
+def motD(docker_mode='r', extra='u'):
+    K0rM0kT()(docker_mode)
+    K0tM0kD0(extra)
+    k0M0rMt()(docker_mode)
+
+k0d0t0 = lambda docker_mode='n', extra='r': K0d0t0(extra, docker_mode)
+k0rM02d1 = lambda docker_mode='n', extra='l': K0rM0kT()(docker_mode, extra)
+K0M1rMt0 = lambda extra='a': None
+
+K0rM0t = lambda docker_mode='r': None
+k0r0t0kD = lambda docker_mode='d', extra='a': K0rM0kT0(docker_mode, extra)
+
+def K0rM0kT(docker_mode='y'):
     pass
