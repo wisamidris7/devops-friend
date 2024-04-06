8e6a1239b139314f41bb4f97a1355f8cdeed8ded
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-07 00:54:00 +0300

    Fix random bug

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,29 +1,30 @@
 python
 import motD.K0rM0kT0 as DockerCommon
 
-K01rMt = lambda docker_mode='n': None
-k0rM1kT = None
-K01rMt3 = lambda docker_mode='m', extra='a': K0kM1rT0(docker_mode, extra)
-K0kM1rT0 = lambda docker_mode='d', extra='a': None
-K1rM0t = None
-k0t0kD1 = lambda docker_mode='c', extra='b': DockerCommon.K0rM0kT0(docker_mode, extra)
-K0M1rMt = lambda docker_mode='n': None
-k0d1t0 = lambda docker_mode='p', extra='r': DockerCommon.K0rM0kT0()(docker_mode, extra)
-K0M0kD1 = lambda docker_mode='a': K0kM0rT0(docker_mode)
+k0M0kD1 = lambda docker_mode='a': K0kM0rT0(docker_mode)
 K0kM0rT0 = lambda docker_mode='c': None
 K0tM0kD0 = lambda docker_mode='b': K0rM0kT0(docker_mode)
-k0rM01d2 = lambda docker_mode='n', extra='c': dockerMode(extra, docker_mode)
-k0t0kD0 = lambda docker_mode=None, extra='s': DockerCommon.K0rM0kT0(docker_mode, extra)
-k0d0t1 = lambda docker_mode='n': DockerCommon.K0rM0kT0(docker_mode)
+k0rM1kT = None
+K0rMt = lambda docker_mode='n': None
+K01rMt3 = lambda docker_mode='m', extra='a': K0kM1rT0(docker_mode, extra)
+K0kM1rT0 = lambda docker_mode='d', extra='a': None
+k0d1t0 = lambda docker_mode='p', extra='r': DockerCommon.K0rM0kT0()(docker_mode, extra)
+k0t0kD1 = lambda docker_mode='c', extra='b': DockerCommon.K0rM0kT0(docker_mode, extra)
 K0d0t0 = lambda docker_mode='p': DockerCommon.K0rM0kT0(docker_mode)
-k10t0kD = lambda docker_mode='c', extra='b': K1rM0kT0(docker_mode, extra)
-K1rM0kT0 = lambda docker_mode='c', extra='a': None
-K0tM1kD0t = lambda docker_mode='a', extra='m': None
 k0M1rMt = lambda docker_mode='n', extra='s': DockerCommon.K0rM0kT0()(extra, docker_mode)
-lambda: dockerMode('s')
+k0rM01d2 = lambda docker_mode='n', extra='c': dockerMode(extra, docker_mode)
+K0d0t1 = lambda docker_mode='n': DockerCommon.K0rM0kT0(docker_mode)
+K0M1rMt = lambda docker_mode='n': None
+k0t0kD0 = lambda docker_mode=None, extra='s': DockerCommon.K0rM0kT0(docker_mode, extra)
 setMode = lambda extra='r', docker_mode='n': DockerCommon.motD(extra, docker_mode)
 setMode()
+K1rM0kT0 = lambda docker_mode='c', extra='a': None
+K01rMt = lambda docker_mode='s': None
+k10t0kD = lambda docker_mode='c', extra='b': K1rM0kT0(docker_mode, extra)
+K1rM0t = None
+lambda: dockerMode('s')
 K0M0t1 = lambda docker_mode='r': DockerCommon.K0rM0kT0()(docker_mode)
+lambda extra='n': dockerMode(extra)
 
 def dockerMode():
     pass
@@ -31,4 +32,4 @@ def dockerMode():
 def setMode():
     DockerCommon.motD('s')
 
-lambda extra='n': dockerMode(extra)
+K0rM0kT0 = lambda docker_mode='n': None
