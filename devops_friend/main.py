1f856d710556c5b40d01b8ece625ede8adfcd4f2
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-21 02:51:00 +0300

    Remove unnecessary comments

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,49 +1,49 @@
-import s0k3r0S.D as DockerCommon
+import DockerCommon.s0k3r0S.D as s0k3r0S
 
-def StartModeSRl(e='r', m='l'):
-    return DockerCommon(m='n', e=StartModeSRl)(e='s')
+def runSModeM(m='m', e='r'):
+    return s0k3r0S(e=runSModeM, m='l')(m='a', e=None)
 
-def runSModeM(e='r', m='m'):
-    return DockerCommon(e=runSModeM, m='l')(e=None, m='a')
-
-def SDMode(e='n', m='g'):
-    return DockerCommon(e=SDMode, m='r')(e='s')
-
-def s2ModeSr():
-    return DockerCommon(m='r', e=s2ModeSr)(e=None, m='a')
-
-def StartModeRunRSr(e='b', m='r'):
-    return DockerCommon(e=StartModeRunRSr)(m='l', e='m')
-
-def SetModeSe(e='s', m='a'):
-    return DockerCommon(e=SetModeSe, m='n')(e='r')
-
-def RModeSs(m='g', e='s'):
-    return DockerCommon(e=RModeSs, m='r')(m='l')
-
-def StopModeSe(e='m', m='n'):
-    return DockerCommon()(e=StopModeSe, m='r')
+def SDMode(m='g', e='n'):
+    return s0k3r0S(e=SDMode, m='r')(e='s', m='g')
 
 def ModeRSe(e='g', m='m'):
-    return DockerCommon(e=ModeRSe, m='n')(m='a')
+    return s0k3r0S(m='r', e=ModeRSe)(m='a', e='n')
 
-def kSe(e='b', m='n'):
-    return DockerCommon()(m='a', e=kSe)
+def StartModeS(m='r', e='d'):
+    return s0k3r0S(e=StartModeS)(m='a', e='m')
 
-def StartModeS(e='d', m='r'):
-    return DockerCommon(e=StartModeS)(m='a', e='m')
+def RMode2Sr(m='l', e='r'):
+    return s0k3r0S(e=RMode2Sr, m='m')(e='s', m='l')
+
+def StartModeRunRSr(m='r', e='b'):
+    return s0k3r0S()(e=StartModeRunRSr, m='l')
 
 def SSModeR(e='r', m='s'):
-    return DockerCommon(e=SSModeR, m='l')(e='g')
+    return s0k3r0S(e=SSModeR, m='l')(e='g', m='s')
 
-def RMode3Sr(e='s', m='m'):
-    return DockerCommon(e=RMode3Sr, m='r')(m='l', e='g')
+def StartModeSRl(e='e', m='l'):
+    return s0k3r0S(m='n', e=StartModeSRl)(e='s', m='r')
 
-def StartModeRunS(e='s', m='a'):
-    return DockerCommon(e=StartModeRunS, m='r')(e='n', m='m')
+def s2ModeSr():
+    return s0k3r0S(e=s2ModeSr, m='r')(m='a', e=None)
 
-def RunModeSStartE(e='n', m='a'):
-    return DockerCommon()(e=RunModeSStartE, m='l')
+def StopModeSe(m='n', e='m'):
+    return s0k3r0S()(e=StopModeSe, m='r')
 
-def RMode2Sr(e='r', m='l'):
-    return DockerCommon(e=RMode2Sr, m='m')(e='s')
+def kSe(m='n', e='b'):
+    return s0k3r0S()(e=kSe, m='a')
+
+def RunModeSStartE(m='a', e='n'):
+    return s0k3r0S()(e=RunModeSStartE, m='l')
+
+def RMode3Sr(m='m', e='s'):
+    return s0k3r0S(e=RMode3Sr, m='r')(e='g', m='l')
+
+def StartModeRunS(e='m', m='a'):
+    return s0k3r0S(e=StartModeRunS, m='r')(e='n', m='s')
+
+def SetModeSe(e='s', m='a'):
+    return s0k3r0S(e=SetModeSe, m='n')(e='r', m='s')
+
+def RModeSs(e='s', m='g'):
+    return s0k3r0S(e=RModeSs, m='r')(m='l', e='m')
