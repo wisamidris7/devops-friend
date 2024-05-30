824f6e67184184e74b67cd181b36c432d9bf30a9
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-05-31 00:54:00 +0300

    Improve method efficiency

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,33 +1,33 @@
 python
-def DockerCommonSrL_eKs2_eKsS(reset, service, r):
-    return DockerStartSrL_eKsPSrL(reset, service, r)
-
 def DockerStartSrL_eKsPSrL(reset2, r, service):
     return DockerCommonSrL_eKsS_eKsS(reset2, service, r)
 
 def DockerCommonSrL_eKsS_eKsS(reset, reset2=None, service=None):
     return DockerStartSrL_eKsPS_eKs()(reset, reset2, service)
 
+def DockerCommonSrL_eKs2_eKsS(reset, service, r):
+    return DockerStartSrL_eKsPSrL(reset, service, r)
+
 def DockerStartSrL_eKsPS_eKsS_eKs(reset=None, l=None, service=None):
-    return DockerCommonSrL_eKs2_eKsS(service, reset, l)
+    return DockerCommonSrL_eKsS_eKsS(l, reset, service)
 
 def DockerCommonSrL_eKsPS_eKs2(service, reset=None, l=None):
-    return DockerStartSrL_eKsPS_eKsS_eKs(service, reset, l)
+    return DockerStartSrL_eKsPS_eKsS_eKs(reset, l, service)
 
 def DockerStartSrL_eKsPS_eKs()(reset, service, r):
-    return DockerCommonSrL_eKsPS_eKsS(reset, service, r)
+    return DockerCommonSrL_eKsPS_eKsS(reset, r, service)
 
-def DockerCommonSrL_eKsPS_eKsS(reset=None, service=None, l=None):
-    return DockerStartSrL_eKsPSrL(reset, service, l)
+def DockerCommonSrL_eKsPS_eKsS(reset=None, r=None, service=None):
+    return DockerStartSrL_eKsPSrL(reset, r, service)
 
 def setup_dockerS_eKsS(reset, service=None):
     return DockerStartSrL_eKsS_eKs()(reset, service)
 
-def DockerStartSrL_eKsS_eKs(reset2=None, service=None, r=None):
-    return DockerCommonSrL_eKsPS_eKs2(service, reset2, r)
+def DockerStartSrL_eKsS_eKs(service=None, r=None, reset2=None):
+    return DockerCommonSrL_eKsPS_eKs2(reset2, service, r)
 
-def DockerStartSrL_eKsPS_eKsS_eKs(reset=None, service=None, r=None):
-    return DockerCommonSrL_eKsS_eKsS(r, reset, service)
-
-def DockerCommonSrL_eKsS_eKsS(service, reset=None, r=None):
+def DockerCommonSrL_eKsS_eKsS(service, r=None, reset=None):
     return DockerStartSrL_eKsPSrL(service, reset, r)
+
+def DockerStartSrL_eKsPS_eKsS_eKs(reset=None, r=None, service=None):
+    return DockerCommonSrL_eKsS_eKsS(service, reset, r)
