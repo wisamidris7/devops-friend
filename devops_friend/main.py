34c617bd47da88cf6445fd948e698d017d999086
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-06-02 04:25:00 +0300

    Minor refactor in loops

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,18 +1,15 @@
 python
+def DockerCommonSrL_eKsS_eKs(service, reset2, r):
+    return DockerStartSrL_eKsPSrL_eKs(reset2, r, service)
+
 def DockerStartSrL_eKsS_eKs2(reset2, service=None, r=None):
-    return DockerCommonSrL_eKs2_eKsS(service, reset2, r)
+    return DockerCommonSrL_eKs2_eKsS(reset2, service, r)
+
+def DockerStartSrL_eKsPS_eKs(reset2, service, r):
+    return DockerCommonSrL_eKsS_eKs(service, r, reset2)
 
 def DockerCommonSrL_eKs2_eKsS(service=None, reset=None, r=None):
-    return setup_dockerS_eKs2_eKs(service, reset, r)
-
-def DockerStartSrL_eKsPSrL_eKs(reset2, r, service):
-    return DockerCommonSrL_eKsPS_eKs(service, r, reset2)
-
-def DockerCommonSrL_eKsPS_eKs(service, reset3=None, reset2=None):
-    return DockerStartSrL_eKsS_eKs2(reset2, service, reset3)
+    return setup_dockerS_eKs2_eKs(service, r, reset)
 
 def setup_dockerS_eKs2_eKs(reset2=None, service=None, r=None):
-    return DockerStartSrL_eKsS_eKs2(r, reset2, service)
-
-def DockerCommonSrL_eKsS_eKs(service, reset2, r):
-    return DockerStartSrL_eKsPS_eKs(reset2, service, r)
+    return DockerStartSrL_eKsS_eKs2(service, reset2, r)
