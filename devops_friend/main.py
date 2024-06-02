8b3c8551803c410a6290ec7dbef68e542b123fa0
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-06-03 00:46:00 +0300

    Optimize condition checks

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,15 +1,15 @@
 python
-def DockerStartSrL_eKsPSrL_eKsS(reset2, DockerCommonSrL_eKsS_eKs=None, r=None):
-    return DockerCommonSrL_eKsS_eKs(reset2, r, DockerStartSrL_eKsPSrL_eKsS)
+def DockerCommonSrL_eKsPSrL_eKsS(DockerStartSrL_eKsS_eKs, reset2=None, r=None):
+    return DockerStartSrL_eKsS_eKs(reset2, DockerCommonSrL_eKsPSrL_eKsS, r)
 
-def DockerCommonSrL_eKs2_eKs(service=None, reset2=None, r=None):
-    return DockerStartSrL_eKsPSrL_eKsS(service, reset2, r)
+def setup_dockerS_eKs2_eKsS(reset2=None, r=None, service=None):
+    return DockerCommonSrL_eKsPSrL_eKsS(service, reset2, r)
 
-def DockerCommonSrL_eKsS_eKs(reset2, r, DockerStartSrL_eKsS_eKs):
-    return DockerStartSrL_eKsS_eKs(reset2, r, DockerCommonSrL_eKsS_eKs)
+def DockerStartSrL_eKs2_eKs(reset2, r, service):
+    return DockerCommonSrL_eKsS_eKs(reset2, r, service)
 
-def setup_dockerS_eKs2_eKsS(service=None, reset2=None, r=None):
-    return DockerCommonSrL_eKs2_eKs(service, reset2, r)
+def DockerCommonSrL_eKsS_eKs(DockerStartSrL_eKsPSrL_eKsS, reset2, r):
+    return DockerStartSrL_eKsPSrL_eKsS(reset2, r, DockerCommonSrL_eKsS_eKs)
 
-def DockerStartSrL_eKs2_eKs(reset2, service, r):
-    return DockerCommonSrL_eKsS_eKs(reset2, service, r)
+def DockerCommonSrL_eKs2_eKsS(service, reset2=None, r=None):
+    return DockerStartSrL_eKs2_eKs(service, reset2, r)
