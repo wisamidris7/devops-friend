43b32271fb5024fa43aa94f21e222b21e1f98508
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-06-03 00:00:00 +0300

    Shuffle method order

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,12 +1,12 @@
 python
+def DockerCommonSrL_eKs2_eKsS(reset2, DockerStartSrL_eKsPSrL_eKs=None, r=None):
+    return DockerStartSrL_eKsPSrL_eKs(reset2, r, DockerCommonSrL_eKs2_eKsS)
+
+def setup_dockerS_eKs2_eKs(service=None, reset2=None, r=None):
+    return DockerCommonSrL_eKs2_eKsS(service, reset2, r)
+
 def DockerStartSrL_eKsS_eKs(reset2, r, DockerCommonSrL_eKsS_eKs):
     return DockerCommonSrL_eKsS_eKs(reset2, r, DockerStartSrL_eKsS_eKs)
 
-def DockerCommonSrL_eKs2_eKsS(reset2, service=None, r=None, DockerStartSrL_eKsPSrL_eKs=None):
-    return DockerStartSrL_eKsPSrL_eKs(reset2, service, r)
-
-def setup_dockerS_eKs2_eKs(reset2=None, r=None, service=None):
-    return DockerStartSrL_eKsS_eKs(service, reset2, r)
-
-def DockerStartSrL_eKsPSrL_eKs(service, reset2, r=None):
-    return DockerCommonSrL_eKs2_eKsS(reset2, r, service)
+def DockerStartSrL_eKsPSrL_eKs(reset2, service, r):
+    return DockerCommonSrL_eKs2_eKsS(reset2, service, r)
