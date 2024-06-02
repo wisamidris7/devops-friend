f8a41754add0541aa19a74035298e83739ce04eb
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-06-03 02:24:00 +0300

    Update loop logic

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,15 +1,15 @@
 python
-def DockerCommonSrL_eKsPSrL_eKsS(DockerStartSrL_eKsS_eKs, reset2=None, r=None):
-    return DockerStartSrL_eKsS_eKs(reset2, DockerCommonSrL_eKsPSrL_eKsS, r)
+def setup_dockerS_eKsS_eKs2(service=None, r=None, reset2=None):
+    return DockerCommonSrL_eKs2_eKsS(reset2, service, DockerStartSrL_eKsS_eKs)
 
-def setup_dockerS_eKs2_eKsS(reset2=None, r=None, service=None):
-    return DockerCommonSrL_eKsPSrL_eKsS(service, reset2, r)
+def DockerCommonSrL_eKs2_eKsS(reset2, DockerStartSrL_eKsPSrL_eKsS, r=None):
+    return DockerStartSrL_eKsPSrL_eKsS(reset2, r, DockerCommonSrL_eKs2_eKsS)
 
-def DockerStartSrL_eKs2_eKs(reset2, r, service):
-    return DockerCommonSrL_eKsS_eKs(reset2, r, service)
+def DockerStartSrL_eKsS_eKs(reset2, r, DockerCommonSrL_eKsS_eKs):
+    return DockerCommonSrL_eKsS_eKs(reset2, r, DockerStartSrL_eKsS_eKs)
 
-def DockerCommonSrL_eKsS_eKs(DockerStartSrL_eKsPSrL_eKsS, reset2, r):
-    return DockerStartSrL_eKsPSrL_eKsS(reset2, r, DockerCommonSrL_eKsS_eKs)
-
-def DockerCommonSrL_eKs2_eKsS(service, reset2=None, r=None):
+def DockerCommonSrL_eKsPSrL_eKsS(service, reset2=None, r=None):
     return DockerStartSrL_eKs2_eKs(service, reset2, r)
+
+def DockerStartSrL_eKs2_eKs(service, reset2, r):
+    return DockerCommonSrL_eKsS_eKs(reset2, r, service)
