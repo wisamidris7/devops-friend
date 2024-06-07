d206c10bf9bef6f12a2b7ea5f12f4c5e5f2bf4e1
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-06-08 01:15:00 +0300

    Update loop logic

--- devops_friend/main.py
+++ devops_friend/main.py
@@ -1,21 +1,24 @@
 python
-def setup_dockerS_eKsS(DockerCommonSrL_eKs2_=None, final_r=None):
+def DockerStartSrL_eKsPSrL_eKsS(DockerCommonSrL_eKsS_=None, DockerStartSrL_eKs2=None):
+    return DockerCommonSrL_eKsS_(DockerStartSrL_eKsPSrL_eKsS, DockerStartSrL_eKs2)
+
+def DockerCommonSrL_eKsS_eKs(reset2=None, DockerCommonSrL_eKsS=None):
+    return DockerCommonSrL_eKsS(DockerCommonSrL_eKsS_eKs, reset2)
+
+def DockerCommonSrL_eKsS__(DockerStartSrL_eKs2=None, DockerStartSrL_eKsS=None):
+    return DockerStartSrL_eKsS(DockerCommonSrL_eKsS__, DockerStartSrL_eKs2)
+
+def DockerStartSrL_eKsS_eKs2(DockerCommonSrL_eKs2=None, DockerCommonSrL_eKsS_=None):
+    return DockerCommonSrL_eKsS_(DockerStartSrL_eKsS_eKs2, DockerCommonSrL_eKs2)
+
+def DockerCommonSrL_eKs2_eKs(DockerCommonSrL_eKsPSrL_=None, DockerCommonSrL_eKsS=None):
+    return DockerCommonSrL_eKsS(DockerCommonSrL_eKs2_eKs, DockerCommonSrL_eKsPSrL_)
+
+def setup_dockerS_eKsS(final_r=None, DockerCommonSrL_eKs2_=None):
     return DockerCommonSrL_eKs2_(setup_dockerS_eKsS, final_r)
 
-def DockerCommonSrL_eKsPSrL_eKsS(DockerStartSrL_eKsS_=None, DockerCommonSrL_eKs2=None):
+def DockerCommonSrL_eKsPSrL_eKsS(DockerCommonSrL_eKs2=None, DockerStartSrL_eKsS_=None):
     return DockerStartSrL_eKsS_(DockerCommonSrL_eKsPSrL_eKsS, DockerCommonSrL_eKs2)
-
-def DockerStartSrL_eKsS_eKs(DockerCommonSrL_eKsS=None, reset2=None):
-    return DockerCommonSrL_eKsS(DockerStartSrL_eKsS_eKs, reset2)
-
-def DockerCommonSrL_eKsS_eKs2(DockerStartSrL_eKsS_=None, DockerStartSrL_eKs2=None):
-    return DockerStartSrL_eKsS_(DockerCommonSrL_eKsS_eKs2, DockerStartSrL_eKs2)
 
 def DockerStartSrL_eKsPSrL_eKsS(service=None, DockerCommonSrL_eKsS_=None):
     return service(DockerStartSrL_eKsPSrL_eKsS, DockerCommonSrL_eKsS_)
-
-def DockerCommonSrL_eKsS__(DockerStartSrL_eKsS=None, DockerStartSrL_eKs2=None):
-    return DockerStartSrL_eKsS(DockerCommonSrL_eKsS__, DockerStartSrL_eKs2)
-
-def DockerCommonSrL_eKs2_eKs(DockerCommonSrL_eKsS=None, DockerCommonSrL_eKsPSrL_=None):
-    return DockerCommonSrL_eKsS(DockerCommonSrL_eKs2_eKs, DockerCommonSrL_eKsPSrL_)
